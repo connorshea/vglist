1106c9c811e54d790401eb1cebc33d147c40e344
Connor Shea <connor.james.shea@gmail.com>
2018-12-28 20:54:56 -0700

    Styling improvements.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   devise_for :users
+
   get 'home/index'
 
   root 'home#index'
