85506222ae506d86ab76c4e76059351b1c92a10b
Connor Shea <connor.james.shea@gmail.com>
2018-12-28 18:50:19 -0700

    Add and set up devise.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,9 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # TODO: Change this when deploying to Heroku.
+  config.action_mailer.default_url_options = { host: 'gametracker.herokuapp.com', port: 3000 }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
