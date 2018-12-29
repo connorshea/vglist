84a303e0e45730807ebd95b7823fe26b32bfd557
Connor Shea <connor.james.shea@gmail.com>
2018-12-28 18:17:48 -0700

    Add Home controller.

--- Gemfile
+++ Gemfile
@@ -28,9 +28,6 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
-
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/index'
+
+  root 'home#index'
 end
