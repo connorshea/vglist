89304c2326ffffc72f4a1d8a2bcaf9b251b1e7fc
Connor Shea <connor.james.shea@gmail.com>
2018-12-30 13:35:46 -0700

    Add Kaminari for pagination.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.all
+    @games = Game.order(:id).page params[:page]
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.order(:id).page params[:page]
   end
 
   def show
