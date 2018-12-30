960e6a9df9c975f719f865e3457a6b0a3e7b74ab
Connor Shea <connor.james.shea@gmail.com>
2018-12-30 12:29:27 -0700

    Give games and releases pages basic functionality.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,9 @@
 class GamesController < ApplicationController
   def index
+    @games = Game.all
   end
 
   def show
+    @game = Game.find(params[:id])
   end
 end
--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -1,7 +1,9 @@
 class ReleasesController < ApplicationController
   def index
+    @releases = Release.all
   end
 
   def show
+    @release = Release.find(params[:id])
   end
 end
