84e2bf7b495ecc3d754a8d315fcf25d2e7daa9ff
Connor Shea <connor.james.shea@gmail.com>
2018-12-30 16:48:25 -0700

    Move purchases from the game model to the release model.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,6 @@
 class Game < ApplicationRecord
   has_many :releases
   has_many :platforms, through: :releases
-  has_many :game_purchases
-  has_many :purchasers, through: :game_purchases,
-    class_name: "User", foreign_key: :user_id
 
   validates :name,
     presence: true,
--- app/models/release.rb
+++ app/models/release.rb
@@ -1,6 +1,9 @@
 class Release < ApplicationRecord
   belongs_to :game
   belongs_to :platform
+  has_many :release_purchases
+  has_many :purchasers, through: :release_purchases,
+    class_name: "User", foreign_key: :user_id
 
   validates :name,
     presence: true,
