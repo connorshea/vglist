188774c77bf4ff2622d118a5b234ed75e5be623a
Connor Shea <connor.james.shea@gmail.com>
2018-12-29 18:15:15 -0700

    Add username attribute to users, display it in the header and require it when signing up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # Add username as an accepted key during sign up.
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,20 @@ class User < ApplicationRecord
 
   has_many :release_purchases
   has_many :releases, through: :release_purchases
+
+  validates :username,
+    # Username is required
+    presence: true,
+    # Must be between 4 and 20 characters
+    length: { minimum: 4, maximum: 20 },
+    # Allow letters, numbers, disallow _ and . at the start or end,
+    # disallow _ or . next to each other or themselves.
+    # Based on https://stackoverflow.com/a/12019115/7143763
+    format: /(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])/,
+    uniqueness: {
+      message: ->(object, data) do
+        "#{data[:value]} is taken already!"
+      end
+    }
+
 end
