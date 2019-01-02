5e1ed73896997a171781b75bff7869b56fef489e
Connor Shea <connor.james.shea@gmail.com>
2019-01-01 21:17:25 -0700

    Add tests for create and update in games controller.

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
-
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -10,11 +9,34 @@ RSpec.describe GamesController, type: :controller do
   end
 
   describe "GET #show" do
+    let(:game) { create(:game) }
+
     it "returns http success" do
-      @game = create(:game)
-      get :show, params: { id: @game.id }
+      get :show, params: { id: game.id }
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe "POST #create" do
+    it "returns http found" do
+      post :create, params: {
+        name: "Game",
+        description: "Lorem ipsum"
+      }
+      expect(response).to have_http_status(:found)
+    end
+  end
+
+  describe "POST #update" do
+    let(:game) { create(:game) }
+
+    it "returns http found" do
+      post :update, params: {
+        id: game.id,
+        name: "Game",
+        description: "Lorem ipsum"
+      }
+      expect(response).to have_http_status(:found)
+    end
+  end
 end
