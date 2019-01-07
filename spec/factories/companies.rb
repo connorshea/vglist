066764a016ca4e449675d53414c0c436c7d766c8
Connor Shea <connor.james.shea@gmail.com>
2019-01-06 18:04:42 -0700

    Add some company specs.

---
+++ spec/factories/companies.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :company do
+    name { "Valve Corporation" }
+    description { "Valve Corporation is an American video game developer, publisher and digital distribution company headquartered in Bellevue, Washington." }
+  end
+end
