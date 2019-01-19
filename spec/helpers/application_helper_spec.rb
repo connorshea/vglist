cd75476e5f7ae50ad2d069c9f079273c1f0d275a
Connor Shea <connor.james.shea@gmail.com>
2019-01-18 22:14:28 -0700

    Remove unnecessary comment.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the ApplicationHelper. For example:
-#
-# describe ApplicationHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe ApplicationHelper, type: :helper do
   describe 'flash class' do
     it 'sets the correct class for a notice' do
