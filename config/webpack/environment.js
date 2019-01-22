f967a9de929fe1159986a7f0925c3cbe322a2893
Connor Shea <connor.james.shea@gmail.com>
2019-01-21 19:27:16 -0700

    Add SCSS loader again.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,4 +5,8 @@ const vue =  require('./loaders/vue')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader'
+});
+
 module.exports = environment
