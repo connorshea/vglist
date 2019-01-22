a20d9bb8fa38bf9d9fb19051d66a34f311222a2b
Connor Shea <connor.james.shea@gmail.com>
2019-01-21 20:23:04 -0700

    Remove extra space.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,6 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
-const vue =  require('./loaders/vue')
+const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
