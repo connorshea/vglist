c28309382196620dacf0fe7bc07e05100c5c21f5
Connor Shea <connor.james.shea@gmail.com>
2019-01-27 11:51:25 -0700

    Add a user dropdown to the navbar.

--- app/javascript/src/bulma.js
+++ app/javascript/src/bulma.js
@@ -18,4 +18,39 @@ document.addEventListener('turbolinks:load', () => {
       });
     });
   }
+
+  // Dropdowns
+  let $dropdowns = getAll('.dropdown:not(.is-hoverable)');
+
+  if ($dropdowns.length > 0) {
+    $dropdowns.forEach(function ($el) {
+      $el.addEventListener('click', function (event) {
+        event.stopPropagation();
+        $el.classList.toggle('is-active');
+      });
+    });
+
+    document.addEventListener('click', function (event) {
+      closeDropdowns();
+    });
+  }
+
+  function closeDropdowns() {
+    $dropdowns.forEach(function ($el) {
+      $el.classList.remove('is-active');
+    });
+  }
+
+  // Close dropdowns if ESC pressed
+  document.addEventListener('keydown', function (event) {
+    var e = event || window.event;
+    if (e.keyCode === 27) {
+      closeDropdowns();
+    }
+  });
+
+  // Functions
+  function getAll(selector) {
+    return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+  }
 });
