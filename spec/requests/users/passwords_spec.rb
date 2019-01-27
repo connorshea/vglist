bf3a5d1699e7caf0e00f7407748f26b6cc107f29
Connor Shea <connor.james.shea@gmail.com>
2019-01-26 18:10:09 -0700

    Add passwords and sessions request specs, remove remaining controller specs.

---
+++ spec/requests/users/passwords_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "User Password", type: :request do
+  describe "GET new_user_session_path" do
+    it 'returns http success' do
+      get new_user_session_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "PUT user_password_path" do
+    let(:user) { create(:confirmed_user) }
+
+    it 'reset user password' do
+      password_token = user.send_reset_password_instructions
+      put user_password_path, params: {
+        user: {
+          reset_password_token: password_token,
+          password: 'newpassword',
+          password_confirmation: 'newpassword'
+        }
+      }
+      follow_redirect!
+      expect(response.body).to include('Your password has been changed successfully.')
+    end
+  end
+end
