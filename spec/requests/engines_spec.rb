4b429ff6c496f941db0dc15787e6480625ac83bf
Connor Shea <connor.james.shea@gmail.com>
2019-02-13 21:56:32 -0700

    Add engines request specs.

--- spec/requests/engines_spec.rb
+++ spec/requests/engines_spec.rb
@@ -1,10 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe "Engines", type: :request do
-  describe "GET /engines" do
-    it "works! (now write some real specs)" do
+  describe "GET engines_path" do
+    it "returns http success" do
       get engines_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET engine_path" do
+    let(:engine) { create(:engine) }
+
+    it "returns http success" do
+      get engine_path(id: engine.id)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET new_engine_path" do
+    let(:user) { create(:confirmed_user) }
+
+    it "returns http success" do
+      sign_in(user)
+
+      get new_engine_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET edit_engine_path" do
+    let(:user) { create(:confirmed_user) }
+    let(:engine) { create(:engine) }
+
+    it "returns http success" do
+      sign_in(user)
+
+      get edit_engine_path(engine.id)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "DELETE engine_path" do
+    let(:user) { create(:confirmed_user) }
+    let!(:engine) { create(:engine) }
+
+    it "deletes an engine" do
+      sign_in(user)
+      expect {
+        delete engine_path(id: engine.id)
+      }.to change(Engine, :count).by(-1)
     end
   end
 end
