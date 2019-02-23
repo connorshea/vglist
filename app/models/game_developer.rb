cf6913b8d2cd77cb66b7a68b2f89f16d537528d0
Connor Shea <connor.james.shea@gmail.com>
2019-02-22 20:35:39 -0700

    Add specs for game developer and publishers.
    Also fix lack of uniqueness validation.

--- app/models/game_developer.rb
+++ app/models/game_developer.rb
@@ -1,4 +1,7 @@
 class GameDeveloper < ApplicationRecord
   belongs_to :game
   belongs_to :company
+
+  validates :game_id,
+    uniqueness: { scope: :company_id }
 end
--- app/models/game_publisher.rb
+++ app/models/game_publisher.rb
@@ -1,4 +1,7 @@
 class GamePublisher < ApplicationRecord
   belongs_to :game
   belongs_to :company
+
+  validates :game_id,
+    uniqueness: { scope: :company_id }
 end
