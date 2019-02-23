8e6017c859ac17ac48e618e52c55b140e518ac2b
Connor Shea <connor.james.shea@gmail.com>
2019-02-23 12:58:09 -0700

    Simplify user and game factories using traits.

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -3,10 +3,12 @@ FactoryBot.define do
     name { "Half-Life 2" }
     description { "A 2004 first-person shooter video game created by Valve." }
 
-    factory :game_with_cover do
+    trait :with_cover do
       after(:build) do |game|
         game.cover.attach(io: File.open(Rails.root.join('spec', 'factories', 'images', 'crysis.jpg')), filename: 'crysis.jpg', content_type: 'image/jpg')
       end
     end
+
+    factory :game_with_cover, traits: [:with_cover]
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,38 +5,33 @@ FactoryBot.define do
     sequence(:username) { |n| "johndoe#{n}" }
     bio { "My name is John Doe and I love video games." }
 
-    factory :confirmed_user do
+    trait :confirmed do
       after(:create) { |user| user.confirm }
+    end
 
-      factory :confirmed_user_with_avatar do
-        after(:build) do |user|
-          user.avatar.attach(io: File.open(Rails.root.join('spec', 'factories', 'images', 'avatar.jpg')), filename: 'avatar.jpg', content_type: 'image/jpg')
-        end
+    trait :with_avatar do
+      after(:build) do |user|
+        user.avatar.attach(io: File.open(Rails.root.join('spec', 'factories', 'images', 'avatar.jpg')), filename: 'avatar.jpg', content_type: 'image/jpg')
       end
     end
-  end
 
-  factory :moderator, class: 'User' do
-    sequence(:email) { |n| "moderator#{n}@example.com" }
-    password { "password" }
-    sequence(:username) { |n| "moderator#{n}" }
-    bio { "I am a moderator." }
-    role { :moderator }
-
-    factory :confirmed_moderator do
-      after(:create) { |user| user.confirm }
+    trait :moderator do
+      role { :moderator }
     end
-  end
 
-  factory :admin, class: 'User' do
-    sequence(:email) { |n| "admin#{n}@example.com" }
-    password { "password" }
-    sequence(:username) { |n| "admin#{n}" }
-    bio { "I am an admin." }
-    role { :admin }
-
-    factory :confirmed_admin do
-      after(:create) { |user| user.confirm }
+    trait :admin do
+      role { :admin }
     end
+
+    factory :confirmed_user,                  traits: [:confirmed]
+    factory :confirmed_user_with_avatar,      traits: [:confirmed, :with_avatar]
+
+    factory :moderator,                       traits: [:moderator]
+    factory :confirmed_moderator,             traits: [:confirmed, :moderator]
+    factory :confirmed_moderator_with_avatar, traits: [:confirmed, :moderator, :with_avatar]
+
+    factory :admin,                           traits: [:admin]
+    factory :confirmed_admin,                 traits: [:confirmed, :admin]
+    factory :confirmed_admin_with_avatar,     traits: [:confirmed, :admin, :with_avatar]
   end
 end
