5b3170d839c9b491261bf8b3044bb88d9baaf63d
Connor Shea <connor.james.shea@gmail.com>
2019-03-01 17:53:34 -0700

    Fix migrations for Rails 6.

--- db/migrate/20190103004002_create_genres.rb
+++ db/migrate/20190103004002_create_genres.rb
@@ -8,10 +8,7 @@ class CreateGenres < ActiveRecord::Migration[5.2]
       t.timestamps
     end
 
-    create_join_table :games, :genres do |t|
-      t.integer :game_id
-      t.integer :genre_id
-    end
+    create_join_table :games, :genres
 
     add_index :games_genres, :game_id
     add_index :games_genres, :genre_id
