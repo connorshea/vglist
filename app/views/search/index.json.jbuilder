5db122421999541d65a850452d4cfc820a905183
Connor Shea <connor.james.shea@gmail.com>
2019-03-10 12:58:09 -0600

    Add search request specs and fix a bug with the search controller's JSON response.

--- app/views/search/index.json.jbuilder
+++ app/views/search/index.json.jbuilder
@@ -5,11 +5,8 @@
 #   'Company': [{ id: 1 }, { id: 2 }, { id: 3 }]
 # }
 @searchable_types.each do |type|
-  results_of_type = @search_results.select { |result| result[:searchable_type] == type }
-  array = json.array results_of_type,
-    :id,
-    :content,
-    :searchable_type,
-    :searchable_id
-  json.set! type.to_sym, array
+  json.set! type.to_sym do
+    results_of_type = @search_results.select { |result| result[:searchable_type] == type }
+    json.array! results_of_type, :id, :content, :searchable_type, :searchable_id
+  end
 end
---
+++ spec/requests/search_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "Search", type: :request do
+  describe "GET search_path" do
+    let(:series) { create(:series) }
+
+    it "returns the given series" do
+      get search_path(query: series.name, format: :json)
+      expected_response = { searchable_id: series.id, content: series.name, searchable_type: 'Series' }.stringify_keys
+      expect(JSON.parse(response.body)['Series'].first).to include(expected_response)
+    end
+
+    it "returns no series" do
+      get search_path(query: SecureRandom.alphanumeric(8), format: :json)
+      expected_response = {}
+      %w[Game Series Company Platform Engine Genre].each do |type|
+        expected_response[type] = []
+      end
+      expect(response.body).to eq(expected_response.to_json)
+    end
+
+    it "returns no series when no query is given" do
+      get search_path(format: :json)
+      expected_response = {}
+      %w[Game Series Company Platform Engine Genre].each do |type|
+        expected_response[type] = []
+      end
+      expect(response.body).to eq(expected_response.to_json)
+    end
+  end
+end
