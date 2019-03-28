95e9cfe49abfb72fc111d587e3c79bee1b21cc34
Connor Shea <connor.james.shea@gmail.com>
2019-03-27 21:09:00 -0600

    Remove log redirecting from puma config.

--- config/puma.rb
+++ config/puma.rb
@@ -17,9 +17,6 @@ environment ENV.fetch("RAILS_ENV") { "production" }
 # Set up socket location
 bind "unix://#{shared_dir}/sockets/puma.sock"
 
-# Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
-
 # Set master PID and state locations
 pidfile "#{shared_dir}/pids/puma.pid"
 state_path "#{shared_dir}/pids/puma.state"
