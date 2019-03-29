3dd134a9d6c7fe638042dceb0f6c4a5a6ac9e37a
Connor Shea <connor.james.shea@gmail.com>
2019-03-28 19:08:42 -0600

    Set up email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,19 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.default_url_options = { host: 'vglist.co', port: 3000 }
+
+  # Send email from AWS SES.
+  config.action_mailer.smtp_settings = {
+    address: "email-smtp.us-west-2.amazonaws.com",
+    port: 587,
+    user_name: ENV["SES_SMTP_USERNAME"],
+    password: ENV["SES_SMTP_PASSWORD"],
+    authentication: :login,
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
