205101206a94534bb6c5b7f88dc4f1af0dc16c31
Connor Shea <connor.james.shea@gmail.com>
2019-03-28 19:33:02 -0600

    Add a sender name for emails sent from vglist.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'vglist.co' }
 
   config.action_mailer.default_options = {
-    from: "noreply@vglist.co"
+    from: "VideoGameList <noreply@vglist.co>"
   }
 
   # Send email from AWS SES.
