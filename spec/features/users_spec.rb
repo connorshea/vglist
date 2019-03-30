4f26386469936b4851b0a716382b57be7cacde02
Connor Shea <connor.james.shea@gmail.com>
2019-03-30 17:57:43 -0600

    Add another feature test for the game modal.

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe "Users", type: :feature do
 
   describe "User page" do
-    let!(:game) { create(:game) }
+    let(:game) { create(:game) }
     let(:user) { create(:confirmed_user) }
+    let(:platform) { create(:platform) }
 
     it "user adds game to library", js: true do
       sign_in(user)
@@ -17,7 +18,34 @@ RSpec.describe "Users", type: :feature do
 
       # Make capybara wait until the modal disappears
       expect(page).to have_no_selector('.modal')
-      expect(user.reload.games).to include(game)
+      expect(user.games).to include(game)
+    end
+
+    it "user adds game with everything to library", js: true do
+      sign_in(user)
+      visit(user_path(user))
+
+      click_button('Add a game to your library')
+      # Select the first item in the dropdown by pressing Enter.
+      fill_in('Game', with: game.name).send_keys(:enter)
+      fill_in('Completion Status', with: 'Unplayed').send_keys(:enter)
+      fill_in('Rating', with: '50')
+      fill_in('Hours Played', with: '10')
+      fill_in('Comments', with: 'Lorem ipsum dolor.')
+      fill_in('Platforms', with: platform.name).send_keys(:enter)
+      click_button 'Save changes'
+
+      # Make capybara wait until the modal disappears
+      expect(page).to have_no_selector('.modal')
+      expect(user.game_purchases.first).to have_attributes(
+        game_id: game.id,
+        user_id: user.id,
+        comments: 'Lorem ipsum dolor.',
+        rating: 50,
+        completion_status: 'unplayed',
+        hours_played: 10
+      )
+      expect(GamePurchasePlatform.count).to eq(1)
     end
   end
 end
