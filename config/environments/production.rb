647bc2d6e49cc1338e61cee48e4edf97e46ce89e
Connor Shea <connor.james.shea@gmail.com>
2019-03-29 22:45:22 -0600

    Require master key in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,10 +16,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # TODO: Might need to enable this?
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  config.require_master_key = false
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
