38fe48237c8d8bf6f4e8f816f029507047a151f4
Connor Shea <connor.james.shea@gmail.com>
2019-03-31 00:10:52 -0600

    Enable being added to the Strict-Transport-Security preload list.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,9 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
+  # Enable being added to the Strict-Transport-Security preload list.
+  config.ssl_options = { hsts: { preload: true } }
+
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
