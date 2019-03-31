7171528117c40e0cced7a4aedd597404032924a0
Connor Shea <connor.james.shea@gmail.com>
2019-03-31 16:21:58 -0600

    Drop plpgsql extension, it doesn't work in DigitalOcean's managed database system and I didn't mean to add it.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_164739) do
+ActiveRecord::Schema.define(version: 2019_03_31_222038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
-  enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
