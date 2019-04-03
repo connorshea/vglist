3c2cd62665df7c8e9539ae6517cd0ecdcfbd300f
Connor Shea <connor.james.shea@gmail.com>
2019-04-03 17:49:47 -0600

    Make the game helpers less dumb.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -2,14 +2,10 @@ module GamesHelper
   # Checks if the user has the given game in their library.
   # This is probably pretty slow and should be optimized.
   def game_in_user_library?(game)
-    return true if current_user.game_purchases.find_by(game_id: game.id).present?
-
-    false
+    return current_user.game_purchases.find_by(game_id: game.id).present?
   end
 
   def game_in_user_favorites?(game)
-    return true if current_user.favorites.games.find_by(favoritable_id: game.id).present?
-
-    false
+    return current_user.favorites.games.find_by(favoritable_id: game.id).present?
   end
 end
