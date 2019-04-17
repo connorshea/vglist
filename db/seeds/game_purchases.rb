df5460244eb4d441e2f0cd76800cf87441a85a5b
Connor Shea <connor.james.shea@gmail.com>
2019-04-16 22:01:17 -0600

    Add platforms to game purchase seeder. Resolves #324.

--- db/seeds/game_purchases.rb
+++ db/seeds/game_purchases.rb
@@ -12,6 +12,12 @@ puts "Creating Game Purchases..."
   (1..Game.count).to_a.sample(3).each do |game_id|
     game = Game.find(game_id)
 
+    platforms = []
+    rand(0..3).times.each do
+      platforms << Platform.find(rand(1..Platform.count))
+    end
+    platforms.uniq!
+
     GamePurchase.find_or_create_by!(
       game: game,
       user: user,
@@ -19,7 +25,8 @@ puts "Creating Game Purchases..."
       completion_status: rand(0..5),
       start_date: Faker::Date.between(1.month.ago, 1.day.ago),
       completion_date: Faker::Date.between(1.month.ago, 1.day.ago),
-      comments: Faker::Lorem.sentence
+      comments: Faker::Lorem.sentence,
+      platforms: platforms
     )
   end
 end
