1ad3b88973c6d5b1f0e5db3f3aadb5b04c3ded93
Connor Shea <connor.james.shea@gmail.com>
2019-04-20 16:34:21 -0600

    Make user_id a foreign key in the favorites table.
    Now favorites are deleted at the database-level when a user is deleted.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_20_211124) do
+ActiveRecord::Schema.define(version: 2019_04_20_223028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -240,6 +240,7 @@ ActiveRecord::Schema.define(version: 2019_04_20_211124) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "favorites", "users", on_delete: :cascade
   add_foreign_key "game_developers", "companies", on_delete: :cascade
   add_foreign_key "game_developers", "games", on_delete: :cascade
   add_foreign_key "game_engines", "engines", on_delete: :cascade
