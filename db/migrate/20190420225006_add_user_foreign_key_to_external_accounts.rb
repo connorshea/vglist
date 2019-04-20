074097f9cd7e3575904a9239b6cd05c7924983ea
Connor Shea <connor.james.shea@gmail.com>
2019-04-20 16:51:47 -0600

    Make user_id a foreign key in the external_accounts table.

---
+++ db/migrate/20190420225006_add_user_foreign_key_to_external_accounts.rb
@@ -0,0 +1,5 @@
+class AddUserForeignKeyToExternalAccounts < ActiveRecord::Migration[5.2]
+  def change
+    add_foreign_key :external_accounts, :users, column: :user_id, on_delete: :cascade
+  end
+end
