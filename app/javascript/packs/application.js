e2344d25176e03bfa604a271f193d9ef6ff2e299
Connor Shea <connor.james.shea@gmail.com>
2019-05-04 15:19:51 -0600

    It's fixed... but at what cost.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
 import * as ActiveStorage from 'activestorage';
 import '../src/application.scss';
-import TurbolinksAdapter from '../src/turbolinks-adapter';
+import TurbolinksAdapter from '../src/turbolinks-adapter-better';
 import Vue from 'vue/dist/vue.esm';
 import '@babel/polyfill';
 import * as Sentry from '@sentry/browser';
---
+++ app/javascript/src/turbolinks-adapter-better.js
@@ -0,0 +1,31 @@
+function handleVueDestructionOn(turbolinksEvent, vue) {
+  document.addEventListener(turbolinksEvent, function teardown() {
+    vue.$destroy();
+    document.removeEventListener(turbolinksEvent, teardown);
+  });
+}
+
+function plugin(Vue, options) {
+  // Install a global mixin
+  Vue.mixin({
+    beforeMount: function() {
+      // If this is the root component, we want to cache the original element contents to replace later
+      // We don't care about sub-components, just the root
+      if (this == this.$root) {
+        var destroyEvent =
+          this.$options.turbolinksDestroyEvent || 'turbolinks:before-render';
+        handleVueDestructionOn(destroyEvent, this);
+        this.$originalEl = this.$el.outerHTML;
+      }
+    },
+
+    destroyed: function() {
+      // We only need to revert the html for the root component
+      if (this == this.$root && this.$el) {
+        this.$el.outerHTML = this.$originalEl;
+      }
+    }
+  });
+}
+
+export default plugin;
