63b9d6d873e2f5b49e26e095ee34c42fe05a2993
Connor Shea <connor.james.shea@gmail.com>
2019-05-05 10:44:45 -0600

    Use the game modal when adding a game from the game page.

---
+++ app/javascript/src/components/add-game-to-library.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <button
+      v-if="gamePurchaseExists"
+      class="button is-fullwidth-mobile is-danger"
+    >Remove from library</button>
+    <button
+      v-else
+      @click="activateModal()"
+      class="button is-fullwidth-mobile mr-5 mr-0-mobile"
+    >Add to library</button>
+    <!-- TODO: Add an edit button when the game already exists. -->
+
+    <game-modal
+      v-if="isModalActive"
+      :isActive="isModalActive"
+      :create="gamePurchaseExists"
+      :userId="userId"
+      v-bind="currentGame"
+      v-on:close="deactivateModal"
+      v-on:closeAndRefresh="closeAndRefresh"
+      v-on:create="todoMethod"
+    ></game-modal>
+  </div>
+</template>
+
+<script>
+import GameModal from './game-modal.vue';
+
+export default {
+  name: 'add-game-to-library',
+  components: {
+    GameModal
+  },
+  props: {
+    userId: {
+      type: Number,
+      required: true
+    },
+    gameId: {
+      type: Number,
+      required: true
+    },
+    gamePurchaseExists: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data: function() {
+    return {
+      isModalActive: false,
+      currentGame: {},
+      games: []
+    };
+  },
+  methods: {
+    activateModal(game = {}) {
+      let html = document.querySelector('html');
+      html.classList.add('is-clipped');
+
+      this.doesGamePurchaseExist =
+        Object.entries(game).length > 0 ? false : true;
+      this.currentGame = game;
+      this.isModalActive = true;
+    },
+    deactivateModal() {
+      let html = document.querySelector('html');
+      html.classList.remove('is-clipped');
+
+      this.isModalActive = false;
+    },
+    closeAndRefresh() {
+      this.deactivateModal();
+    },
+    todoMethod() {
+      console.log('todo');
+    },
+    removeGameFromLibrary() {
+      let removeGameFromLibraryPath = `/games/${
+        this.gameId
+      }/remove_game_from_library`;
+
+      let params = {
+        id: this.gameId
+      };
+    }
+  }
+};
+</script>
