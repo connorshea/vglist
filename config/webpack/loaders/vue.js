b10a1c779b081a0c134248bbbbf2ef0a12cb263c
Connor Shea <connor.james.shea@gmail.com>
2019-05-05 16:37:45 -0600

    Change Webpack config to strip whitespace from Vue components.

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -1,6 +1,13 @@
 module.exports = {
   test: /\.vue(\.erb)?$/,
-  use: [{
-    loader: 'vue-loader'
-  }]
-}
+  use: [
+    {
+      loader: 'vue-loader',
+      options: {
+        compilerOptions: {
+          preserveWhitespace: false
+        }
+      }
+    }
+  ]
+};
