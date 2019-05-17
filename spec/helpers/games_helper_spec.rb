ce21df14c4521ef4b50552b794bf60667f124e7f
Connor Shea <connor.james.shea@gmail.com>
2019-05-16 18:10:24 -0600

    Add tests for game_in_user_favorites helper.

--- spec/helpers/games_helper_spec.rb
+++ spec/helpers/games_helper_spec.rb
@@ -15,7 +15,27 @@ RSpec.describe GamesHelper, type: :helper do
 
     it 'returns false when game is not in user library' do
       allow(controller).to receive(:current_user).and_return(user)
+      game_purchase
       expect(helper.game_in_user_library?(game)).to be(false)
+    end
+  end
+
+  describe 'game in user favorites' do
+    let(:game) { create(:game) }
+    let(:user) { create(:confirmed_user) }
+    let(:user_with_favorite) { create(:confirmed_user) }
+    let(:favorite_game) { create(:favorite_game, user: user_with_favorite, game: game) }
+
+    it 'returns true when game is in user favorites' do
+      allow(controller).to receive(:current_user).and_return(user_with_favorite)
+      favorite_game
+      expect(helper.game_in_user_favorites?(game)).to be(true)
+    end
+
+    it 'returns false when game is not in user favorites' do
+      allow(controller).to receive(:current_user).and_return(user)
+      favorite_game
+      expect(helper.game_in_user_favorites?(game)).to be(false)
     end
   end
 end
