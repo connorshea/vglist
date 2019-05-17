0ffa6db9603a7261b4632c92b8a2187b1cca7afe
Connor Shea <connor.james.shea@gmail.com>
2019-05-16 19:29:43 -0600

    Add MobyGames IDs to games.

--- app/models/game.rb
+++ app/models/game.rb
@@ -89,6 +89,13 @@ class Game < ApplicationRecord
       greater_than: 0
     }
 
+  validates :mobygames_id,
+    uniqueness: true,
+    allow_nil: true,
+    format: %r{\A[a-z-_0-9]+/?[a-z-_0-9]+\z},
+    # Allow up to 300 characters just in case there's some game with an incredibly long name.
+    length: { maximum: 300 }
+
   # Include games in global search.
   multisearchable against: [:name]
 
