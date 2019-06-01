61d07ca0a7a65367dcdcf8bf2973b508aa6a9c7b
Connor Shea <connor.james.shea@gmail.com>
2019-06-01 14:15:40 -0600

    Disable HelperInstanceVariable cop in SettingsHelper.

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -5,10 +5,10 @@ module SettingsHelper
   end
 
   def resource
-    resource || User.new
+    @resource || User.new
   end
 
   def devise_mapping
-    devise_mapping || Devise.mappings[:user]
+    @devise_mapping || Devise.mappings[:user]
   end
 end
