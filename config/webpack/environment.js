3956ceee227532dd1c2d02d6bcb7e1e52732dda8
Connor Shea <connor.james.shea@gmail.com>
2019-06-19 18:45:18 -0600

    Prettify environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,14 @@
-const { environment } = require('@rails/webpacker')
-const typescript =  require('./loaders/typescript')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+const { environment } = require('@rails/webpacker');
+const typescript = require('./loaders/typescript');
+const { VueLoaderPlugin } = require('vue-loader');
+const vue = require('./loaders/vue');
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin());
+environment.loaders.prepend('vue', vue);
 
 environment.loaders.get('sass').use.splice(-1, 0, {
   loader: 'resolve-url-loader'
 });
 
-environment.loaders.prepend('typescript', typescript)
-module.exports = environment
+environment.loaders.prepend('typescript', typescript);
+module.exports = environment;
