6ac5da5d858e16c7cf7339056c119c8be9395e4d
Connor Shea <connor.james.shea@gmail.com>
2019-06-19 19:03:15 -0600

    Convert vue-loader to TypeScript.
    Add a Vue type definition shim and update tsconfig to target esnext.

---
+++ app/javascript/src/vue-shims.d.ts
@@ -0,0 +1,6 @@
+// Make Single File Components work with TypeScript.
+// See https://github.com/microsoft/TypeScript-Vue-Starter/tree/c243b11a6f827e780a5163999bc472c95ff5a0e0#single-file-components
+declare module "*.vue" {
+  import Vue from "vue";
+  export default Vue;
+}
