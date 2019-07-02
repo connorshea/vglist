e5241f09212a869e87d282ed6843d37a7ebf862a
Connor Shea <connor.james.shea@gmail.com>
2019-07-01 19:14:19 -0600

    Add signatures to Faker initializer.

--- config/initializers/faker.rb
+++ config/initializers/faker.rb
@@ -3,26 +3,34 @@ if defined?(Faker)
   module GameProperties
     class Game < Faker::Base
       class << self
+        extend T::Sig
+        
+        sig { returns(String) }
         def name
           fetch('game.name')
         end
 
+        sig { returns(String) }
         def platform
           fetch('game.platform')
         end
 
+        sig { returns(String) }
         def genre
           fetch('game.genre')
         end
 
+        sig { returns(String) }
         def company
           fetch('game.company')
         end
 
+        sig { returns(String) }
         def engine
           fetch('game.engine')
         end
 
+        sig { returns(String) }
         def series
           fetch('game.series')
         end
