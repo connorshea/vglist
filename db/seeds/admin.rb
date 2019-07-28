685efa4211d13d22ee0b3082fef508a53bfefa7b
Connor Shea <connor.james.shea@gmail.com>
2019-07-28 14:43:05 -0600

    Autocorrect some Sorbet type errors.

--- db/seeds/admin.rb
+++ db/seeds/admin.rb
@@ -13,7 +13,7 @@ User.create_with(
 admin = User.find_by(email: "admin@example.com")
 
 # Confirm the admin's email.
-admin.confirm
+T.must(admin).confirm
 
 # Exit early if there aren't at least 10 games in the db.
 return if Game.count < 10
