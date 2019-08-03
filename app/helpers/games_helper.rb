860d348e0274b6619dbbbf5f46eef2d5526068df
Connor Shea <connor.james.shea@gmail.com>
2019-08-02 22:27:00 -0600

    Add types to GamesHelper.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,15 +1,20 @@
 # typed: false
 module GamesHelper
+  extend T::Sig
+
   # Checks if the user has the given game in their library.
   # This is probably pretty slow and should be optimized.
+  sig { params(game: Game).returns(T::Boolean) }
   def game_in_user_library?(game)
     return current_user.game_purchases.find_by(game_id: game.id).present?
   end
 
+  sig { params(game: Game).returns(T::Boolean) }
   def game_in_user_favorites?(game)
     return current_user.favorite_games.find_by(game_id: game.id).present?
   end
 
+  sig { params(sort: T.nilable(Symbol), humanized_name: String).returns(T.untyped) }
   def sort_dropdown_link(sort, humanized_name)
     # Merge the params to preserve any other filters.
     if sort.nil?
