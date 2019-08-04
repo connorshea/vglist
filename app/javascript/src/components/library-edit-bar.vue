d08e1741517fb559fe948e4d23fc178f75bc3976
Connor Shea <connor.james.shea@gmail.com>
2019-08-03 22:16:14 -0600

    Allow setting Rating via the edit bar.

--- app/javascript/src/components/library-edit-bar.vue
+++ app/javascript/src/components/library-edit-bar.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="library-edit-bar level">
     <div class="level-left">
-      <span class="has-text-weight-bold">{{ selectedGamesString }}</span>
+      <span class="has-text-weight-bold pr-15">{{ selectedGamesString }}</span>
+      <number-field
+        :form-class="''"
+        :attribute="'rating'"
+        :placeholder="'Rating (out of 100)'"
+        :required="false"
+        :max="100"
+        v-model="updateData.rating"
+      ></number-field>
     </div>
     <div class="level-right">
       <button
         class="button is-fullwidth-mobile mr-5 mr-0-mobile"
         @click="$emit('closeEditBar')"
       >Cancel</button>
-      <button class="button is-fullwidth-mobile mr-5 mr-0-mobile" @click="updateGames">Update</button>
+      <button
+        class="button is-fullwidth-mobile mr-5 mr-0-mobile"
+        :disabled="!updateButtonActive"
+        @click="updateGames"
+      >Update</button>
     </div>
   </div>
 </template>
@@ -16,12 +28,12 @@
 <script lang="ts">
 import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
-import SingleSelect from './fields/single-select.vue';
+import NumberField from './fields/number-field.vue';
 
 export default {
   name: 'library-edit-bar',
   components: {
-    SingleSelect
+    NumberField
   },
   props: {
     gamePurchases: {
@@ -43,6 +55,10 @@ export default {
       this.gamePurchases.forEach(gamePurchase => {
         this.updateData['ids'].push(gamePurchase.id);
       });
+
+      if (this.updateData['rating'] === '') {
+        delete this.updateData['rating'];
+      }
 
       fetch('/game_purchases/bulk_update.json', {
         method: 'POST',
@@ -67,6 +83,26 @@ export default {
     selectedGamesString() {
       let gpLength = this.gamePurchases.length;
       return `${gpLength} game${gpLength > 1 ? 's' : ''} selected`;
+    },
+    updateButtonActive() {
+      if (Object.keys(this.updateData).length === 1) {
+        return false;
+      }
+
+      let returnBool = false;
+
+      // Check if either rating or completion status have values.
+      // If they do, return true. Otherwise, return false.
+      ['rating', 'completion_status'].forEach(attribute => {
+        if (
+          typeof this.updateData[attribute] !== 'undefined' &&
+          this.updateData[attribute] !== ''
+        ) {
+          returnBool = true;
+        }
+      });
+
+      return returnBool;
     }
   }
 };
