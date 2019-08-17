2e7495f43fec0535be7a384d1fd27ee690d4568c
Connor Shea <connor.james.shea@gmail.com>
2019-08-16 21:10:27 -0600

    Appease the rubopig.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -26,7 +26,7 @@ end
 # If you are using UJS then enable automatic nonce generation
 Rails.application.config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
 
-Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
+Rails.application.config.content_security_policy_nonce_directives = %w[script-src]
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
