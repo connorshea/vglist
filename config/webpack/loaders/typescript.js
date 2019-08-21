64df203b20aa8b1bb9a23877cd0e78144a952221
Connor Shea <connor.james.shea@gmail.com>
2019-08-20 18:15:34 -0600

    Make TypeScript compilation much faster.

--- config/webpack/loaders/typescript.js
+++ config/webpack/loaders/typescript.js
@@ -6,7 +6,14 @@ module.exports = {
     {
       loader: 'ts-loader',
       options: PnpWebpackPlugin.tsLoaderOptions({
-        appendTsSuffixTo: [/\.vue$/]
+        appendTsSuffixTo: [/\.vue$/],
+
+        // Using `fork-ts-checker-webpack-plugin` See:
+        // https://github.com/TypeStrong/fork-ts-checker-webpack-plugin
+        // to run type checking in a separate process, so `ts-loader` only does the
+        // compilation work. Reduces compile time like crazy. See also:
+        // https://github.com/TypeStrong/ts-loader#faster-builds
+        transpileOnly: true
       })
     }
   ]
