a483cc643edaac5d0fa1c91ccd30dcadf9c69ace
Connor Shea <connor.james.shea@gmail.com>
2019-08-26 21:53:01 -0600

    Appease the Rubocop.

--- db/migrate/20190827015409_rename_game_purchase_events_to_events.rb
+++ db/migrate/20190827015409_rename_game_purchase_events_to_events.rb
@@ -6,12 +6,14 @@ class RenameGamePurchaseEventsToEvents < ActiveRecord::Migration[6.0]
     add_column :events, :eventable_type, :string
     rename_column :events, :event_type, :event_category
     Event.reset_column_information
+    # rubocop:disable Rails/SkipsModelValidations
     Event.update_all(eventable_type: "GamePurchase")
+    # rubocop:enable Rails/SkipsModelValidations
 
     add_index :events, [:eventable_id, :eventable_type, :user_id],
       name: :index_events_on_id_type_and_user_id
   end
-  
+
   def down
     rename_table :events, :game_purchase_events
     rename_column :events, :eventable_id, :game_purchase_id
