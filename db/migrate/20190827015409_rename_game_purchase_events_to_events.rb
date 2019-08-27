05cb9426796b9fc21c5e5f7c2c5ea03a23dd518a
Connor Shea <connor.james.shea@gmail.com>
2019-08-26 22:16:36 -0600

    Add a comment explaining the migration for renaming game_purchase_events.

--- db/migrate/20190827015409_rename_game_purchase_events_to_events.rb
+++ db/migrate/20190827015409_rename_game_purchase_events_to_events.rb
@@ -1,5 +1,7 @@
 # typed: true
 class RenameGamePurchaseEventsToEvents < ActiveRecord::Migration[6.0]
+  # Turn the game_purchase_events table into a table named events and make
+  # it polymorphic to support other event types.
   def up
     rename_table :game_purchase_events, :events
     rename_column :events, :game_purchase_id, :eventable_id
