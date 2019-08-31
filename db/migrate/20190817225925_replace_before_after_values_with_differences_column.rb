44c02e29b421dd038fc8a7692f2ad05453d642b3
Connor Shea <connor.james.shea@gmail.com>
2019-08-30 19:47:18 -0600

    Remove unnecessary disabled cop.

--- db/migrate/20190817225925_replace_before_after_values_with_differences_column.rb
+++ db/migrate/20190817225925_replace_before_after_values_with_differences_column.rb
@@ -1,7 +1,4 @@
 # typed: true
-# rubocop:disable Rails/BulkChangeTable
-# There's a bug in rubocop where remove_column is recommended to be replaced by
-# this cop, even though its replacement wouldn't be reversible.
 class ReplaceBeforeAfterValuesWithDifferencesColumn < ActiveRecord::Migration[6.0]
   def change
     remove_column :game_purchase_events, :before_value, :text
@@ -9,4 +6,3 @@ class ReplaceBeforeAfterValuesWithDifferencesColumn < ActiveRecord::Migration[6.
     add_column :game_purchase_events, :differences, :jsonb
   end
 end
-# rubocop:enable Rails/BulkChangeTable
