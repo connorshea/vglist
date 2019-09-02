a7e6c4aca213653a9bdb0ca64182b8393952a967
Connor Shea <connor.james.shea@gmail.com>
2019-09-02 15:04:32 -0600

    Remove gif as a valid cover content type.
    Only 3 games had gifs, so it's not worth the trouble.

--- app/models/game.rb
+++ app/models/game.rb
@@ -63,7 +63,7 @@ class Game < ApplicationRecord
 
   validates :cover,
     attached: false,
-    content_type: ['image/png', 'image/jpg', 'image/jpeg', 'image/gif'],
+    content_type: ['image/png', 'image/jpg', 'image/jpeg'],
     size: { less_than: 4.megabytes }
 
   validates :wikidata_id,
