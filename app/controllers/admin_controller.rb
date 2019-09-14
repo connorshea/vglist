eaf0a0f1467c61004c82e57c9b4f7d0c9aec07cf
Connor Shea <connor.james.shea@gmail.com>
2019-09-14 14:17:28 -0600

    Add record counts to the Admin dashboard.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,6 +5,19 @@ class AdminController < ApplicationController
   def dashboard
     # We don't have anything specific to validate, so just pass nil.
     authorize nil, policy_class: AdminPolicy
+
+    @counts = {
+      users: User.count,
+      games: Game.count,
+      platforms: Platform.count,
+      series: Series.count,
+      engines: Engine.count,
+      companies: Company.count,
+      genres: Genre.count,
+      events: Event.count,
+      game_purchases: GamePurchase.count,
+      relationships: Relationship.count
+    }
   end
 
   def wikidata_blocklist
