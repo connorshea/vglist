0241502f56cbb9c5b1c6592dfff3e19b819a280a
Connor Shea <connor.james.shea@gmail.com>
2019-09-22 15:19:50 -0600

    Add search for models besides Game.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -16,9 +16,19 @@ module Types
       argument :id, ID, required: true
     end
 
+    field :series_search, [SeriesType], null: true do
+      description "Find a series by searching based on its name."
+      argument :query, String, required: true, description: "Name to search by."
+    end
+
     field :company, CompanyType, null: true do
       description "Find a company by ID."
       argument :id, ID, required: true
+    end
+
+    field :company_search, [CompanyType], null: true do
+      description "Find a company by searching based on its name."
+      argument :query, String, required: true, description: "Name to search by."
     end
 
     field :platform, PlatformType, null: true do
@@ -26,14 +36,29 @@ module Types
       argument :id, ID, required: true
     end
 
+    field :platform_search, [PlatformType], null: true do
+      description "Find a platform by searching based on its name."
+      argument :query, String, required: true, description: "Name to search by."
+    end
+
     field :engine, EngineType, null: true do
-      description "Find an engine by ID."
+      description "Find a game engine by ID."
       argument :id, ID, required: true
+    end
+
+    field :engine_search, [EngineType], null: true do
+      description "Find a game engine by searching based on its name."
+      argument :query, String, required: true, description: "Name to search by."
     end
 
     field :genre, GenreType, null: true do
       description "Find a genre by ID."
       argument :id, ID, required: true
+    end
+
+    field :genre_search, [GenreType], null: true do
+      description "Find a genre by searching based on its name."
+      argument :query, String, required: true, description: "Name to search by."
     end
 
     field :user, UserType, null: true do
@@ -53,20 +78,40 @@ module Types
       Series.find(id)
     end
 
+    def series_search(query:)
+      Series.search(query)
+    end
+
     def company(id:)
       Company.find(id)
+    end
+
+    def company_search(query:)
+      Company.search(query)
     end
 
     def platform(id:)
       Platform.find(id)
     end
 
+    def platform_search(query:)
+      Platform.search(query)
+    end
+
     def engine(id:)
       Engine.find(id)
     end
 
+    def engine_search(query:)
+      Engine.search(query)
+    end
+
     def genre(id:)
       Genre.find(id)
+    end
+
+    def genre_search(query:)
+      Genre.search(query)
     end
 
     def user(id:)
