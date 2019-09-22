34f33e830754ac9b2055f94a7a9fd5328b625d75
Connor Shea <connor.james.shea@gmail.com>
2019-09-22 17:17:06 -0600

    Improve documentation for GamePurchaseType.

--- app/graphql/types/game_purchase_type.rb
+++ app/graphql/types/game_purchase_type.rb
@@ -1,13 +1,15 @@
 # typed: true
 module Types
   class GamePurchaseType < Types::BaseObject
+    description "This represents a game that a user has in their library. It includes data like the user's rating for the game, comments, hours played, etc."
+
     field :id, ID, null: false
     field :game, GameType, null: false
     field :user, UserType, null: false
     field :comments, String, null: true
-    field :rating, Integer, null: true
+    field :rating, Integer, null: true, description: "Rating out of 100."
     field :hours_played, Float, null: true
-    field :platforms, [PlatformType], null: true
+    field :platforms, [PlatformType], null: true, description: "Platforms that the user owns this game on."
 
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false, description: "When this game purchase was first created."
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false, description: "When this game purchase was last updated."
