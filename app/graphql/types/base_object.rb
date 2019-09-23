89d0890ddab364efdeb8e8b0e15a13b9e30c1356
Connor Shea <connor.james.shea@gmail.com>
2019-09-22 19:05:25 -0600

    Add an authorization check for the base object.
    This prevents users that are not logged-in from using the API.

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -1,6 +1,13 @@
-# typed: strong
+# typed: true
 module Types
   class BaseObject < GraphQL::Schema::Object
     include Pundit
+
+    # User needs to be logged in to get anything from the API.
+    def self.authorized?(_object, context)
+      raise GraphQL::ExecutionError, "You must be logged in to use the API." if context[:current_user].nil?
+
+      return true
+    end
   end
 end
