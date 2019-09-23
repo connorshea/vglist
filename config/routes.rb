a7b0e51c83251577fa8c28e70f3536f826d2fe4b
Connor Shea <connor.james.shea@gmail.com>
2019-09-22 19:08:02 -0600

    Move API routes to the bottom of routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 # typed: strict
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
-  post "/graphql", to: "graphql#execute"
   # Put this at the top for ~performance~
   root 'home#index'
 
@@ -95,4 +91,10 @@ Rails.application.routes.draw do
   end
 
   get '/about', to: 'static_pages#about'
+
+  # API routes
+  # Add the GraphiQL IDE in development mode.
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
+  # Execute GraphQL queries posted to '/graphql'.
+  post "/graphql", to: "graphql#execute"
 end
