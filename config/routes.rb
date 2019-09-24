33b4284d2b24e40edbdb2616ba0bfcc44c9b9e8e
Connor Shea <connor.james.shea@gmail.com>
2019-09-23 18:35:19 -0600

    Restrict the GraphQL endpoint to development for now.

--- config/routes.rb
+++ config/routes.rb
@@ -95,6 +95,7 @@ Rails.application.routes.draw do
   # API routes
   # Add the GraphiQL IDE in development mode.
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
+  # TODO: Enable this in production when the authentication stuff has been built.
   # Execute GraphQL queries posted to '/graphql'.
-  post "/graphql", to: "graphql#execute"
+  post "/graphql", to: "graphql#execute" if Rails.env.development?
 end
