78dbd6c5e65e72a4efca085d7c61a1b8631701ab
Connor Shea <connor.james.shea@gmail.com>
2019-09-28 23:51:33 -0600

    Add another test for the GraphQL route.

--- spec/requests/graphql_spec.rb
+++ spec/requests/graphql_spec.rb
@@ -1,14 +1,20 @@
 # typed: false
 require 'rails_helper'
 
-RSpec.describe "Graphql", type: :request do
+RSpec.describe "GraphQL", type: :request do
   describe "POST graphql_path" do
     let(:user) { create(:confirmed_user) }
     let(:application) { create(:application, owner: user) }
     let(:token) { create(:access_token, application: application, resource_owner: user) }
 
-    it 'responds with http success' do
+    it 'responds with http success if you use an access token' do
       post graphql_path(format: :json, access_token: token.token)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'responds with http success if user is logged in' do
+      sign_in(user)
+      post graphql_path(format: :json)
       expect(response).to have_http_status(:success)
     end
   end
