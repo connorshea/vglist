d9b7c0d625d36e248255e838363694fec705aded
Connor Shea <connor.james.shea@gmail.com>
2019-10-05 22:18:56 -0600

    Add tests for game covers and user avatars in API.

--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -4,10 +4,10 @@ require 'rails_helper'
 RSpec.describe "Users API", type: :request do
   describe "Query for data on users" do
     let(:user) { create(:confirmed_user) }
+    let(:user_with_avatar) { create(:confirmed_user_with_avatar) }
 
     it "returns basic data for user" do
       sign_in(user)
-      user
       query_string = <<-GRAPHQL
         query($id: ID!) {
           user(id: $id) {
@@ -15,6 +15,7 @@ RSpec.describe "Users API", type: :request do
             username
             role
             privacy
+            avatarUrl
           }
         }
       GRAPHQL
@@ -29,7 +30,34 @@ RSpec.describe "Users API", type: :request do
           "id" => user.id.to_s,
           "username" => user.username,
           "role" => user.role.upcase,
-          "privacy" => user.privacy.upcase
+          "privacy" => user.privacy.upcase,
+          "avatarUrl" => nil
+        }
+      )
+    end
+
+    it "returns avatar for user" do
+      sign_in(user_with_avatar)
+      query_string = <<-GRAPHQL
+        query($id: ID!) {
+          user(id: $id) {
+            id
+            username
+            avatarUrl
+          }
+        }
+      GRAPHQL
+
+      result = VideoGameListSchema.execute(
+        query_string,
+        context: { current_user: user_with_avatar },
+        variables: { id: user_with_avatar.id }
+      )
+      expect(result.to_h["data"]["user"]).to eq(
+        {
+          "id" => user_with_avatar.id.to_s,
+          "username" => user_with_avatar.username,
+          "avatarUrl" => Rails.application.routes.url_helpers.rails_blob_url(user_with_avatar.avatar_attachment, only_path: true)
         }
       )
     end
