af29ce3cba8921c4e8cd62bf3ae0ba0b836e1c0f
Connor Shea <connor.james.shea@gmail.com>
2019-10-06 19:11:05 -0600

    Fix problem with Doorkeeper trying to authenticate as admin.

--- app/controllers/oauth/applications_controller.rb
+++ app/controllers/oauth/applications_controller.rb
@@ -1,8 +1,12 @@
-# typed: false
+# typed: true
 class Oauth::ApplicationsController < Doorkeeper::ApplicationsController
   before_action :authenticate_user!
-  after_action :verify_authorized, only: :index
+  # Doorkeeper tries to require that the user be an admin in order to create
+  # an application, so we skip it to allow 'normal' users to create OAuth
+  # apps.
+  skip_before_action :authenticate_admin!
 
+  # Only display applications owned by the current user.
   def index
     skip_authorization
     skip_policy_scope
@@ -11,14 +15,25 @@ class Oauth::ApplicationsController < Doorkeeper::ApplicationsController
 
   # Every application must have some owner
   def create
+    skip_authorization
     @application = Doorkeeper::Application.new(application_params)
-    @application.owner = current_user if Doorkeeper.configuration.confirm_application_owner?
+    @application.owner = current_user if T.unsafe(Doorkeeper).configuration.confirm_application_owner?
     if @application.save
       flash[:notice] = I18n.t(:notice, scope: [:doorkeeper, :flash, :applications, :create])
       redirect_to oauth_application_url(@application)
     else
       render :new
     end
+  end
+
+  def new
+    skip_authorization
+    super
+  end
+
+  def show
+    skip_authorization
+    super
   end
 
   private
