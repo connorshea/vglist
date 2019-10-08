92199d568f286c5bcfb08ed2e30e964f759a8d44
Connor Shea <connor.james.shea@gmail.com>
2019-10-07 21:15:20 -0600

    Return nil for events if the user isn't visible.

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -20,6 +20,8 @@ module Types
     field :avatar_url, String, null: true, description: "URL for the user's avatar image. `null` means the user has the default avatar."
 
     def events
+      return nil unless user_visible?
+
       Event.recently_created
            .joins(:user)
            .where(user_id: @object.id)
@@ -37,7 +39,7 @@ module Types
     # Extremely cursed metaprogramming that protects private users from having their details exposed
     # if the UserPolicy wants to prevent it.
     def handler(field_name)
-      return @object.public_send(field_name) if user_public?
+      return @object.public_send(field_name) if user_visible?
 
       nil
     end
@@ -52,7 +54,7 @@ module Types
       end
     end
 
-    def user_public?
+    def user_visible?
       # Short-circuit if the user has a public account, to prevent instantiating
       # a UserPolicy and all that.
       return @object.public_account? || UserPolicy.new(@context[:current_user], @object).show?
