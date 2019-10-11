2e8349329229398d7bab69fb3304007522226603
Connor Shea <connor.james.shea@gmail.com>
2019-10-10 19:53:12 -0600

    Add more specs for activity.

--- spec/requests/api/activity_spec.rb
+++ spec/requests/api/activity_spec.rb
@@ -4,7 +4,10 @@ require 'rails_helper'
 RSpec.describe "Activity API", type: :request do
   describe "Query for data on activity globally" do
     let(:user) { create(:confirmed_user) }
+    let(:user2) { create(:confirmed_user) }
     let(:game_purchase) { create(:game_purchase, user: user) }
+    let(:relationship) { create(:relationship, follower: user, followed: user2) }
+    let(:favorite_game) { create(:favorite_game, user: user) }
 
     it "returns basic data for activity events" do
       sign_in(user)
@@ -26,6 +29,7 @@ RSpec.describe "Activity API", type: :request do
         query_string,
         context: { current_user: user }
       )
+
       expect(result.to_h["data"]["activity"]).to eq(
         [
           {
@@ -44,6 +48,93 @@ RSpec.describe "Activity API", type: :request do
               "__typename" => "User"
             }
           }
+        ]
+      )
+    end
+
+    it "returns data for various types of activity events" do
+      sign_in(user)
+      game_purchase
+      relationship
+      favorite_game
+
+      query_string = <<-GRAPHQL
+        query {
+          activity(feedType: GLOBAL) {
+            user {
+              username
+            }
+            eventable {
+              __typename
+              ... on User {
+                id
+              }
+              ... on GamePurchase {
+                id
+              }
+              ... on Relationship {
+                id
+              }
+              ... on FavoriteGame {
+                id
+              }
+            }
+          }
+        }
+      GRAPHQL
+
+      result = VideoGameListSchema.execute(
+        query_string,
+        context: { current_user: user }
+      )
+
+      expect(result.to_h["data"]["activity"]).to eq(
+        [
+          {
+            "user" => {
+              "username" => user.username
+            },
+            "eventable" => {
+              "__typename" => "FavoriteGame",
+              "id" => favorite_game.id.to_s
+            }
+          },
+          {
+            "user" => {
+              "username" => user.username
+            },
+            "eventable" => {
+              "__typename" => "Relationship",
+              "id" => relationship.id.to_s
+            }
+          },
+          {
+            "user" => {
+              "username" => user2.username
+            },
+            "eventable" => {
+              "__typename" => "User",
+              "id" => user2.id.to_s
+            }
+          },
+          {
+            "user" => {
+              "username" => user.username
+            },
+            "eventable" => {
+              "__typename" => "GamePurchase",
+              "id" => game_purchase.id.to_s
+            }
+          },
+          {
+            "user" => {
+              "username" => user.username
+            },
+            "eventable" => {
+              "__typename" => "User",
+              "id" => user.id.to_s
+            }
+          },
         ]
       )
     end
