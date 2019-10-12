61be3a907087da5fef05ef27c8a1fe0f7523306a
Connor Shea <connor.james.shea@gmail.com>
2019-10-12 14:40:30 -0600

    Update TODO comment.

--- app/graphql/types/page_info_type.rb
+++ app/graphql/types/page_info_type.rb
@@ -2,7 +2,8 @@
 class Types::PageInfoType < GraphQL::Types::Relay::PageInfo
   field :total, Integer, null: false, description: "Total number of items for this result set."
 
-  # TODO: Make this work properly (right now the )
+  # TODO: Make this work properly (right now the nodes are only the nodes on
+  # the current page, rather than all the nodes returned by the query)
   def total
     object.nodes.size
   end
