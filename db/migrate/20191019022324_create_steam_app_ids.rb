b43db39d5e77b4437bcc9d654aede7039ea29b9f
Connor Shea <connor.james.shea@gmail.com>
2019-10-19 16:28:39 -0600

    Remove timestamps from steam_app_ids.

--- db/migrate/20191019022324_create_steam_app_ids.rb
+++ db/migrate/20191019022324_create_steam_app_ids.rb
@@ -1,12 +1,13 @@
 # typed: true
 class CreateSteamAppIds < ActiveRecord::Migration[6.0]
   def change
+    # No need for timestamps on this table.
+    # rubocop:disable Rails/CreateTableWithTimestamps
     create_table :steam_app_ids do |t|
       t.references :game, null: false, foreign_key: true
       t.integer :app_id, null: false
-
-      t.timestamps
     end
+    # rubocop:enable Rails/CreateTableWithTimestamps
 
     add_index :steam_app_ids, :app_id, unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# typed: false
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -286,8 +285,6 @@ ActiveRecord::Schema.define(version: 2019_10_19_022324) do
   create_table "steam_app_ids", force: :cascade do |t|
     t.bigint "game_id", null: false
     t.integer "app_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.index ["app_id"], name: "index_steam_app_ids_on_app_id", unique: true
     t.index ["game_id"], name: "index_steam_app_ids_on_game_id"
   end
