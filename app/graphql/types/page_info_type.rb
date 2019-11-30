278d7644c512a387b98102da2d9ad83e04e9582d
Connor Shea <connor.james.shea@gmail.com>
2019-11-29 23:00:57 -0700

    Improve the description for page_size in pageInfo.

--- app/graphql/types/page_info_type.rb
+++ app/graphql/types/page_info_type.rb
@@ -1,6 +1,6 @@
 # typed: true
 class Types::PageInfoType < GraphQL::Types::Relay::PageInfo
-  field :page_size, Integer, null: false, description: "page size"
+  field :page_size, Integer, null: false, description: "The max page size for a given set of nodes."
 
   def page_size
     VideoGameListSchema.default_max_page_size
