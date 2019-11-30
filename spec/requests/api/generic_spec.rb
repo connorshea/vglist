056be0c25e736c6d40e413dbe450a1dc2a4bf5e0
Connor Shea <connor.james.shea@gmail.com>
2019-11-29 22:47:46 -0700

    Add a test for some pagination functionality.

---
+++ spec/requests/api/generic_spec.rb
@@ -0,0 +1,34 @@
+# typed: false
+require 'rails_helper'
+
+RSpec.describe "API", type: :request do
+  describe "Query for data on game_purchase" do
+    let(:user) { create(:confirmed_user) }
+    let(:application) { build(:application, owner: user) }
+    let(:access_token) { create(:access_token, resource_owner_id: user.id, application: application) }
+    let(:games) { create_list(:game, 31)}
+
+    it "returns correct hasNextPage with 31 records" do
+      games
+      query_string = <<-GRAPHQL
+        query {
+          games {
+            nodes {
+              id
+              name
+            }
+            pageInfo {
+              hasNextPage
+              pageSize
+            }
+          }
+        }
+      GRAPHQL
+
+      result = api_request(query_string, token: access_token)
+
+      expect(result["data"]["games"]["pageInfo"]["hasNextPage"]).to eq(true)
+      expect(result["data"]["games"]["pageInfo"]["pageSize"]).to eq(30)
+    end
+  end
+end
