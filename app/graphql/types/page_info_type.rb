b32b1744864193a1faaea4783318c81c64c6c599
Connor Shea <connor.james.shea@gmail.com>
2020-02-02 13:07:47 -0700

    Add a type signature to page size method.

--- app/graphql/types/page_info_type.rb
+++ app/graphql/types/page_info_type.rb
@@ -1,7 +1,10 @@
-# typed: true
+# typed: strict
 class Types::PageInfoType < GraphQL::Types::Relay::PageInfo
+  extend T::Sig
+
   field :page_size, Integer, null: false, description: "The max page size for a given set of nodes."
 
+  sig { returns(Integer) }
   def page_size
     VideoGameListSchema.default_max_page_size
   end
