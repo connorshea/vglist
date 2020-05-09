fee9d95d6e51fd0bdbb27018d947d9f4a53818bc
Connor Shea <connor.james.shea@gmail.com>
2020-05-08 20:16:14 -0600

    Upgrade to Webpacker 5.1.
    This doesn't seem to break anything, which is surprising?

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,21 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require("path");
+
+// Makes TypeScript compilation time much faster.
+environment.plugins.append(
+  'ForkTsCheckerWebpackPlugin',
+  new ForkTsCheckerWebpackPlugin({
+    tsconfig: path.resolve(__dirname, "../../tsconfig.json"),
+    vue: true,
+
+    // makes webpack wait for type checking to finish before "emitting"; allows
+    // type errors to remain present in the build output from webpack and in
+    // webpack-dev-server's darkened overlay
+    async: false
+  })
+);
 
 module.exports = environment.toWebpackConfig()
