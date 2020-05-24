444210ea8341e4183e7ae864f6b022a18c3e3764
Connor Shea <connor.james.shea@gmail.com>
2020-05-24 15:11:44 -0600

    Remove graphiql_override.

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -11,11 +11,9 @@ class Mutations::BaseMutation < GraphQL::Schema::Mutation
   sig { params(_args: T.untyped).returns(T::Boolean) }
   def ready?(**_args)
     # Make sure the doorkeeper scopes include write.
-    # Skip this check if the user is using token authentication or
-    # if the request comes from GraphiQL.
+    # Skip this check if the user is using token authentication.
     raise GraphQL::ExecutionError, "Your token must have the 'write' scope to perform a mutation." if !context[:token_auth] &&
-                                                                                                      !context[:doorkeeper_scopes]&.include?('write') &&
-                                                                                                      !context[:graphiql_override]
+                                                                                                      !context[:doorkeeper_scopes]&.include?('write')
 
     return true
   end
