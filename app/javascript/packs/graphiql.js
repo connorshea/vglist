d0eeeafd04ffc80dc87b5db49a5e16f1c10c7566
Connor Shea <connor.james.shea@gmail.com>
2020-05-24 13:58:50 -0600

    Move graphiql, react, and react-dom into the package.json.
    Rather than vendoring them, this seems like the best solution overall.
    Much simpler and much cleaner :)

--- app/javascript/packs/graphiql.js
+++ app/javascript/packs/graphiql.js
@@ -3,10 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-// NOTE: This pack is NOT meant for actual use.
-// Its purpose is to get webpacker to find these files so they can be loaded
-// via the `asset_pack_path` method. (The CSS pack _is_ used, though.)
-import '../src/vendor/graphiql-1.0.0-alpha.10.css';
-import '../src/vendor/react-16.13.1.static.js';
-import '../src/vendor/react-dom-16.13.1.static.js';
-import '../src/vendor/graphiql-1.0.0-alpha.10.static.js';
+import 'graphiql/graphiql.min.css';
+import 'graphiql/graphiql.min.js';
+
+require.context('react/umd/', true);
+require.context('react-dom/umd/', true);
