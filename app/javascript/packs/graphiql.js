f1294d5a1ec3fe640df7ecac169ef3689f145370
Connor Shea <connor.james.shea@gmail.com>
2020-05-24 12:22:36 -0600

    Remove unnecessary code and add a comment to graphiql.js.

--- app/javascript/packs/graphiql.js
+++ app/javascript/packs/graphiql.js
@@ -3,6 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// NOTE: This pack is NOT meant for actual use.
+// Its purpose is to get webpacker to find these files so they can be loaded
+// via the `asset_pack_path` method.
 import '../src/vendor/graphiql-1.0.0-alpha.10.css';
 import '../src/vendor/react-16.13.1.static.js';
 import '../src/vendor/react-dom-16.13.1.static.js';
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,23 +1,12 @@
 const { environment } = require('@rails/webpacker');
 const { VueLoaderPlugin } = require('vue-loader');
 const vue = require('./loaders/vue');
-const { resolve } = require('path');
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin());
 environment.loaders.prepend('vue', vue);
 
 environment.loaders.get('sass').use.splice(-1, 0, {
   loader: 'resolve-url-loader'
-});
-
-environment.config.merge({
-  resolve:{
-    alias: {
-      'react': resolve('app/javascript/src/vendor/react-16.13.1'),
-      'react-dom': resolve('app/javascript/src/vendor/react-dom-16.13.1'),
-      'graphiql': resolve('app/javascript/src/vendor/graphiql-1.0.0-alpha.10')
-    }
-  }
 });
 
 module.exports = environment;
