c38e6baa3b653d9b1b95c421e711907a7b7a246c
Connor Shea <connor.james.shea@gmail.com>
2020-05-23 21:38:54 -0600

    Remove graphiql-rails and replace it with the npm package directly.
    Load the JavaScript packages directly so Webpacker picks them up properly.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,10 +13,3 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-
-if Rails.env.development?
-  Rails.application.config.assets.precompile += [
-    "graphiql/rails/application.css",
-    "graphiql/rails/application.js"
-  ]
-end
