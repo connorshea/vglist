f912f62a3c66da42b4c554eb97c5680854e301d3
Connor Shea <connor.james.shea@gmail.com>
2020-06-16 20:03:33 -0600

    Move GraphiQL injection to a JavaScript file rather than an inline script.

--- app/javascript/src/graphiql-loader.js
+++ app/javascript/src/graphiql-loader.js
@@ -1,5 +1,23 @@
 import { GraphiQL } from 'graphiql';
 import { buildSchema } from 'graphql';
 
-window.GraphiQL = GraphiQL;
-window.buildSchema = buildSchema;
+let graphQLFetcher = (graphQLParams, opts = { headers: {} }) => {
+  return fetch(window.location.origin + '/graphql', {
+    method: 'post',
+    headers: Object.assign({ 'Content-Type': 'application/json', 'User-Agent': 'vglist.co GraphiQL' }, opts.headers),
+    body: JSON.stringify(graphQLParams),
+  }).then(response => response.json());
+}
+
+let schema = document.getElementById("graphiql-injection-point").dataset.graphqlSchema;
+let graphQLSchema = buildSchema(schema);
+
+ReactDOM.render(
+  React.createElement(GraphiQL, {
+    schema: graphQLSchema,
+    fetcher: graphQLFetcher,
+    headerEditorEnabled: true,
+    headers: '{\n  "X-User-Email": "user@example.com",\n  "X-User-Token": "API_TOKEN_HERE"\n}'
+  }),
+  document.getElementById("graphiql-injection-point")
+);
