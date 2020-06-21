26d84661d557d1783ce37c32236c80bb5dfa678b
Connor Shea <connor.james.shea@gmail.com>
2020-06-20 23:59:58 -0600

    Fix the fork-ts-checker config for v5.

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -6,15 +6,18 @@ const path = require("path");
 
 // Makes TypeScript compilation time much faster.
 environment.plugins.append(
-  'ForkTsCheckerWebpackPlugin',
+  "ForkTsCheckerWebpackPlugin",
   new ForkTsCheckerWebpackPlugin({
-    tsconfig: path.resolve(__dirname, "../../tsconfig.json"),
-    vue: true,
-
+    typescript: {
+      tsconfig: path.resolve(__dirname, "../../tsconfig.json"),
+      extensions: {
+        vue: true
+      }
+    },
     // makes webpack wait for type checking to finish before "emitting"; allows
     // type errors to remain present in the build output from webpack and in
     // webpack-dev-server's darkened overlay
-    async: false
+    async: false,
   })
 );
 
