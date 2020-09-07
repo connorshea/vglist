1dbbcc248464c19423270c89f52e4a4269ea43dd
Connor Shea <connor.james.shea@gmail.com>
2020-09-07 17:20:06 -0600

    Make some minor accessibility improvements.

--- app/javascript/src/bulma.ts
+++ app/javascript/src/bulma.ts
@@ -27,7 +27,9 @@ let initBulma = () => {
         const target = el.dataset.target;
         const $target = document.getElementById(target);
 
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        // Toggle the "is-active" class on both the "navbar-burger" and the
+        // "navbar-menu", and toggle the aria-expanded attribute.
+        el.setAttribute('aria-expanded', (el.getAttribute('aria-expanded') === 'false').toString());
         el.classList.toggle('is-active');
         $target.classList.toggle('is-active');
       });
