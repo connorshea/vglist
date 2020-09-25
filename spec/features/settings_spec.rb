feb124aa6bb479e59c8681e632144977932dd62b
Connor Shea <connor.james.shea@gmail.com>
2020-09-24 22:52:29 -0600

    Add a test for the honeypot test.
    Delicious.

--- spec/features/settings_spec.rb
+++ spec/features/settings_spec.rb
@@ -42,6 +42,25 @@ RSpec.describe "Settings", type: :feature do
       expect(page).to have_content('Your account has been updated successfully.')
     end
 
+    # Fill in the honeypot field and make sure it doesn't succeed in updating
+    # the user.
+    it "with user filling in honeypot" do
+      sign_in(user)
+
+      visit(settings_account_path)
+      within('#new-user') do
+        fill_in('user[password]', with: 'newpassword')
+        fill_in('user[password_confirmation]', with: 'newpassword')
+        fill_in('user[current_password]', with: user.password)
+        # Capybara won't find this field unless you tell it to check fields
+        # that are not visible.
+        fill_in('user[honey]', with: 'pooh', visible: false)
+      end
+      click_button 'Update'
+
+      expect(page).not_to have_content('Your account has been updated successfully.')
+    end
+
     it "with no user" do
       visit(settings_account_path)
       expect(page).not_to have_current_path(settings_account_path)
