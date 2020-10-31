f26e4da1152b1b7483ce1449bbbc8caae6ea1431
Connor Shea <connor.james.shea@gmail.com>
2020-10-31 16:48:33 -0600

    Simplify the code for the Statistic model.

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,24 +1,28 @@
-# typed: strict
+# typed: true
 class Statistic < ApplicationRecord
-  validates :users, numericality: true
-  validates :games, numericality: true
-  validates :platforms, numericality: true
-  validates :series, numericality: true
-  validates :engines, numericality: true
-  validates :companies, numericality: true
-  validates :genres, numericality: true
-  validates :stores, numericality: true
-  validates :events, numericality: true
-  validates :game_purchases, numericality: true
-  validates :relationships, numericality: true
-  validates :games_with_covers, numericality: true
-  validates :games_with_release_dates, numericality: true
-  validates :banned_users, numericality: true
-  validates :mobygames_ids, numericality: true
-  validates :pcgamingwiki_ids, numericality: true
-  validates :wikidata_ids, numericality: true
-  validates :giantbomb_ids, numericality: true
-  validates :steam_app_ids, numericality: true
-  validates :epic_games_store_ids, numericality: true
-  validates :gog_ids, numericality: true
+  STATISTICS = [
+    :users,
+    :games,
+    :platforms,
+    :series,
+    :engines,
+    :companies,
+    :genres,
+    :stores,
+    :events,
+    :game_purchases,
+    :relationships,
+    :games_with_covers,
+    :games_with_release_dates,
+    :banned_users,
+    :mobygames_ids,
+    :pcgamingwiki_ids,
+    :wikidata_ids,
+    :giantbomb_ids,
+    :steam_app_ids,
+    :epic_games_store_ids,
+    :gog_ids
+  ].freeze
+
+  validates(*STATISTICS, numericality: true)
 end
