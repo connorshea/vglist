7f6e67e7b39c7277c427ca32ae79cff71a95c720
Connor Shea <connor.james.shea@gmail.com>
2020-11-27 13:09:37 -0700

    Add require.rb for tapioca.

---
+++ sorbet/tapioca/require.rb
@@ -0,0 +1,11 @@
+# typed: false
+
+# Add your extra requires here
+require 'sparql'
+require 'sparql/client'
+require 'ruby-progressbar'
+require 'simplecov'
+require 'capybara/rspec'
+require 'pundit/rspec'
+require 'pundit/matchers'
+require 'active_storage_validations/matchers'
