350711d0380ff18c818476986fe66f067a9c55f1
Connor Shea <connor.james.shea@gmail.com>
2020-11-28 16:21:40 -0700

    Allow updating games' hours played value from Steam.
    This updates the backend to support updating existing game purchases
    if necessary.

--- app/services/steam_import_service.rb
+++ app/services/steam_import_service.rb
@@ -4,9 +4,10 @@ require 'set'
 class SteamImportService
   extend T::Sig
 
-  sig { params(user: User).void }
-  def initialize(user)
+  sig { params(user: User, update_hours: T::Boolean).void }
+  def initialize(user:, update_hours: false)
     @user = user
+    @update_hours = update_hours
   end
 
   class Error < StandardError; end
@@ -23,26 +24,30 @@ class SteamImportService
 
     raise NoGamesError if games.nil?
 
-    blocklisted_steam_app_ids = SteamBlocklist.pluck(:steam_app_id)
+    blocklisted_steam_app_ids = T.cast(SteamBlocklist.pluck(:steam_app_id), T::Array[Integer])
 
     games.reject! { |game| game['img_logo_url'].blank? }
 
-    steam_ids = T.cast(
-      games.map { |g| g['appid'].to_i }.uniq,
-      T::Array[Integer]
-    )
-    stored_ids = T.cast(SteamAppId.where(app_id: steam_ids).pluck(:app_id, :game_id), T::Array[[Integer, Integer]])
-    stored_id_hash = stored_ids.each_with_object({}) do |(app_id, game_id), hash|
+    # The Steam IDs for all games that were just imported from Steam.
+    steam_ids = T.cast(games.map { |g| g['appid'].to_i }.uniq, T::Array[Integer])
+
+    # An array of ID pairs for the Steam App ID and vglist game ID, lists all
+    # the games that were sent from the Steam API that could be found in our
+    # database.
+    matching_app_and_game_ids = T.cast(SteamAppId.where(app_id: steam_ids).pluck(:app_id, :game_id), T::Array[[Integer, Integer]])
+    # A hash that stores all the Steam App IDs and Game IDs.
+    matching_app_and_game_ids_hash = matching_app_and_game_ids.each_with_object({}) do |(app_id, game_id), hash|
       hash[app_id] = game_id
     end
+    # Get a list of Steam IDs that weren't found in the vglist database.
     missing_ids = T.cast(
-      steam_ids.to_set - stored_ids.map(&:first).to_set,
+      steam_ids.to_set - matching_app_and_game_ids.map(&:first).to_set,
       T::Set[Integer]
     )
 
     create_time = Time.current
 
-    attrs =
+    game_hashes =
       games.each
            .lazy
            .reject { |game| missing_ids.include?(game['appid']) }
@@ -50,20 +55,32 @@ class SteamImportService
         hours_played = (game_info['playtime_forever'].to_f / 60).round(1)
         {
           hours_played: hours_played,
-          game_id: stored_id_hash[game_info['appid']],
+          game_id: matching_app_and_game_ids_hash[game_info['appid']],
           user_id: user.id,
           created_at: create_time,
           updated_at: create_time
         }
+      end.to_a
+
+    created = []
+    updated = []
+    if game_hashes.any?
+      game_hashes.each do |game_hash|
+        game_purchase = GamePurchase.find_by(game_hash.slice(:game_id, :user_id))
+        if game_purchase.nil?
+          created << GamePurchase.create(game_hash)
+        # If we're intending to update the hours played value, update it!
+        elsif @update_hours
+          # Skip if there's no change in the hours played.
+          next if game_hash[:hours_played] == game_purchase[:hours_played]
+
+          game_purchase.update(hours_played: game_hash[:hours_played])
+          updated << game_purchase.reload
+        end
       end
-    attrs = attrs.to_a
-    added_purchases =
-      if attrs.any?
-        inserted = attrs.map { |attr| GamePurchase.find_or_create_by(attr) }
-        GamePurchase.where(id: inserted.map { |e| e['id'] })
-      else
-        GamePurchase.none
-      end
+    end
+    created_purchases = GamePurchase.where(id: created.map { |e| e['id'] })
+    updated_purchases = GamePurchase.where(id: updated.map { |e| e['id'] })
 
     unmatched = missing_ids.to_a.map do |id|
       game = games.find { |g| g['appid'] == id }
@@ -77,7 +94,8 @@ class SteamImportService
     end.compact
 
     Result.new(
-      added: added_purchases,
+      created: created_purchases,
+      updated: updated_purchases,
       unmatched: unmatched
     )
   end
@@ -90,15 +108,24 @@ class SteamImportService
   class Result < T::Struct
     extend T::Sig
 
-    const :added, GamePurchase::RelationType
+    const :created, GamePurchase::RelationType
+    const :updated, GamePurchase::RelationType
     const :unmatched, T::Array[Unmatched]
 
+    # Returns the games for all the newly created game purchases.
     sig { returns(Game::RelationType) }
     def added_games
-      Game.joins(:game_purchases).merge(added)
+      Game.joins(:game_purchases).merge(created)
+    end
+
+    # Returns the games for all the updated game purchases.
+    sig { returns(Game::RelationType) }
+    def updated_games
+      Game.joins(:game_purchases).merge(updated)
     end
   end
 
+  sig { returns(User) }
   attr_reader :user
 
   sig { returns(String) }
@@ -106,8 +133,9 @@ class SteamImportService
     "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=#{ENV['STEAM_WEB_API_KEY']}&steamid=#{steam_account_id}&include_appinfo=1&include_played_free_games=1"
   end
 
+  sig { returns(Integer) }
   def steam_account_id
-    steam_account.try(:[], :steam_id)
+    T.must(steam_account)[:steam_id]
   end
 
   sig { returns(T.nilable(ExternalAccount)) }
