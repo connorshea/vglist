4f16186e2f737d8a1dfddcfc760006b38a6c1bae
Connor Shea <connor.james.shea@gmail.com>
2021-01-21 19:07:34 -0700

    Add paper_trail RSpec helpers and disable it when running tests.

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -22,6 +22,10 @@ RSpec.describe Company, type: :model do
     end
   end
 
+  describe "Versioning" do
+    it { should be_versioned }
+  end
+
   describe "Associations" do
     it { should have_many(:game_developers) }
     it { should have_many(:developed_games).through(:game_developers).source(:game) }
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'selenium/webdriver'
 require 'webdrivers'
+require 'paper_trail/frameworks/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
