1bf65524500bd5796dcc358bb43540a9bbc3b873
Connor Shea <connor.james.shea@gmail.com>
2021-01-24 14:44:09 -0700

    Use validations for some GraphQL queries.
    And add tests for them.

--- spec/support/api_request_test_helper.rb
+++ spec/support/api_request_test_helper.rb
@@ -1,6 +1,10 @@
 # typed: false
 module ApiRequestTestHelper
   def api_request(query_string, variables: nil, token:)
+    # Allow variables to be sent as snake case, e.g. "giantbomb_id" by
+    # transforming them into "giantbombId" before passing them to GraphQL.
+    variables = variables.transform_keys { |key| key.to_s.camelize(:lower) } unless variables.nil?
+
     post graphql_path,
       params: {
         query: query_string,
