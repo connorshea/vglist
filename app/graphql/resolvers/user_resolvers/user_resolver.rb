3db0e4ecd2823289c62e344b67fc23990a9f274d
Connor Shea <connor.james.shea@gmail.com>
2021-02-21 16:18:22 -0700

    Add slug argument to UserResolver.

--- app/graphql/resolvers/user_resolvers/user_resolver.rb
+++ app/graphql/resolvers/user_resolvers/user_resolver.rb
@@ -4,23 +4,34 @@ module Resolvers
     class UserResolver < Resolvers::BaseResolver
       type Types::UserType, null: true
 
-      description "Find a user. May only use one or the other."
+      description "Find a user. May only use one argument at a time."
 
       argument :id, ID, required: false, description: "Find a user by their ID."
       argument :username, String, required: false, description: "Find a user by their username."
+      argument :slug, String, required: false, description: "Find a user by their slug (their sanitized username, used in URLs for user pages)."
 
       # Use validator to validate that one of the arguments is being used.
       validates required: {
-        one_of: [:id, :username],
+        one_of: [:id, :username, :slug],
         message: 'Cannot provide more than one argument to user at a time.'
       }
 
-      sig { params(id: T.nilable(T.any(String, Integer)), username: T.nilable(String)).returns(T.nilable(User)) }
-      def resolve(id: nil, username: nil)
+      sig do
+        params(
+          id: T.nilable(T.any(String, Integer)),
+          username: T.nilable(String),
+          slug: T.nilable(String)
+        ).returns(
+          T.nilable(User)
+        )
+      end
+      def resolve(id: nil, username: nil, slug: nil)
         if !id.nil?
-          User.find(id)
+          User.find_by(id: id)
         elsif !username.nil?
           User.find_by(username: username)
+        elsif !slug.nil?
+          User.find_by(slug: slug)
         end
       end
     end
