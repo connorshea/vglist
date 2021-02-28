f48703422a3613d5b6722fa938d617ef1b394162
Connor Shea <connor.james.shea@gmail.com>
2021-02-27 22:19:21 -0700

    Add filtering by year and platform to game list resolver.

--- app/graphql/resolvers/game_resolvers/list_resolver.rb
+++ app/graphql/resolvers/game_resolvers/list_resolver.rb
@@ -7,10 +7,25 @@ module Resolvers
       description "List all games."
 
       argument :sort_by, Types::GameSortType, required: false, description: "The order to sort the games in, if any."
+      argument :on_platform, ID, required: false, description: "Filter games by the ID of the platform they're on."
+      argument :by_year, Integer,
+        required: false,
+        description: "Filter games by the year of their release date, earliest allowed year is 1950.",
+        validates: { numericality: { greater_than_or_equal_to: 1950 } }
 
-      sig { params(sort_by: T.nilable(String)).returns(Game::RelationType) }
-      def resolve(sort_by: nil)
+      sig do
+        params(
+          sort_by: T.nilable(String),
+          on_platform: T.nilable(String),
+          by_year: T.nilable(Integer)
+        ).returns(Game::RelationType)
+      end
+      def resolve(sort_by: nil, on_platform: nil, by_year: nil)
         games = Game.all
+
+        games = games.on_platform(on_platform) unless on_platform.nil?
+        games = games.by_year(by_year) unless by_year.nil?
+
         sort_by.nil? ? games.order(:id) : games.public_send(sort_by.to_sym)
       end
     end
