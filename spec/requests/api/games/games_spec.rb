00f472d49b46fc1193739eb593c27556fb0e0e5b
Connor Shea <connor.james.shea@gmail.com>
2021-02-28 10:59:41 -0700

    Add tests for the sorting options in the games query.

--- spec/requests/api/games/games_spec.rb
+++ spec/requests/api/games/games_spec.rb
@@ -6,36 +6,71 @@ RSpec.describe "Games API", type: :request do
     let(:user) { create(:confirmed_user) }
     let(:application) { build(:application, owner: user) }
     let(:access_token) { create(:access_token, resource_owner_id: user.id, application: application) }
-    let(:game) { create(:game) }
-    let(:game2) { create(:game) }
 
-    it "returns data for games when listing" do
-      game
-      game2
-      query_string = <<-GRAPHQL
-        query {
-          games {
-            nodes {
-              id
-              name
+    context 'with basic games query' do
+      let!(:game) { create(:game) }
+      let!(:game2) { create(:game) }
+
+      it "returns data for games when listing" do
+        query_string = <<-GRAPHQL
+          query {
+            games {
+              nodes {
+                id
+                name
+              }
             }
           }
-        }
-      GRAPHQL
+        GRAPHQL
 
-      result = api_request(query_string, token: access_token)
-      expect(result.graphql_dig(:games, :nodes)).to eq(
-        [
-          {
-            id: game.id.to_s,
-            name: game.name
-          },
-          {
-            id: game2.id.to_s,
-            name: game2.name
-          }
-        ]
-      )
+        result = api_request(query_string, token: access_token)
+        expect(result.graphql_dig(:games, :nodes)).to eq(
+          [
+            {
+              id: game.id.to_s,
+              name: game.name
+            },
+            {
+              id: game2.id.to_s,
+              name: game2.name
+            }
+          ]
+        )
+      end
+    end
+
+    context 'when sorting games' do
+      let(:games) { create_list(:game, 3) }
+
+      # Test each sort order and make sure it matches the corresponding sort
+      # method. We test the sort scopes individually in method specs, so we
+      # should be able to rely on those tests for testing the specific order.
+      [:newest, :oldest, :recently_updated, :least_recently_updated, :most_favorites, :most_owners, :recently_released, :highest_avg_rating].each do |sort_order|
+        it "returns data in expected order for #{sort_order}" do
+          query_string = <<-GRAPHQL_WITH_INTERP
+            query {
+              games(sortBy: #{sort_order.upcase}) {
+                nodes {
+                  id
+                  name
+                }
+              }
+            }
+          GRAPHQL_WITH_INTERP
+
+          games
+          result = api_request(query_string, token: access_token)
+          # This is a bit of a hack to get the games in the same format as
+          # they're returned by GraphQL (hashes with only the specific
+          # requested keys, with IDs being strings).
+          expected_games = Game.public_send(sort_order).select(:id, :name).map(&:as_json).map(&:symbolize_keys).map do |game|
+            game[:id] = game[:id].to_s
+            game
+          end
+
+          expect(result.graphql_dig(:games, :nodes)).to eq(expected_games)
+        end
+      end
     end
   end
 end
