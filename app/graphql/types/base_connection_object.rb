5ced772597c855975d04d9ff558a264c509b2262
Connor Shea <connor.james.shea@gmail.com>
2021-03-01 16:45:57 -0700

    Add totalCount to GraphQL's basic connection object.
    The last time I tried to implement this, the docs were outdated and wrong :D

--- app/graphql/types/base_connection_object.rb
+++ app/graphql/types/base_connection_object.rb
@@ -1,4 +1,12 @@
 # typed: strict
 class Types::BaseConnectionObject < GraphQL::Types::Relay::BaseConnection
+  extend T::Sig
+
   field :page_info, Types::PageInfoType, null: false, description: "Information to aid in pagination."
+  field :total_count, Integer, null: false, description: "The total number of records returned by this query."
+
+  sig { returns(Integer) }
+  def total_count
+    object.items.size
+  end
 end
