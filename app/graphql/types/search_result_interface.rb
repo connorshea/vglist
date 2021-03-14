15db732caa87176f6cff725247810f1f95a7dc08
Connor Shea <connor.james.shea@gmail.com>
2021-03-14 17:37:09 -0600

    Add missing descriptions to appease schema linter.

--- app/graphql/types/search_result_interface.rb
+++ app/graphql/types/search_result_interface.rb
@@ -3,6 +3,8 @@ module Types
   module SearchResultInterface
     include Types::BaseInterface
 
+    description 'Search results returned by global search.'
+
     field :id, ID, null: false, description: 'The ID of the SearchResult record, probably not very useful most of the time.'
     field :content, String, null: false, description: 'The contents of the search result, typically the name or username of the corresponding record.'
     field :searchable_type, SearchableEnum, null: false, description: "The type of the corresponding record."
--- app/graphql/types/searchable_enum.rb
+++ app/graphql/types/searchable_enum.rb
@@ -3,12 +3,12 @@ module Types
   class SearchableEnum < Types::BaseEnum
     description "The types of records that can be returned as a `SearchResult`."
 
-    value "COMPANY", value: 'Company'
-    value "ENGINE", value: 'Engine'
-    value "GAME", value: 'Game'
-    value "GENRE", value: 'Genre'
-    value "PLATFORM", value: 'Platform'
-    value "SERIES", value: 'Series'
-    value "USER", value: 'User'
+    value "COMPANY", value: 'Company', description: 'Self-explanatory.'
+    value "ENGINE", value: 'Engine', description: 'Self-explanatory.'
+    value "GAME", value: 'Game', description: 'Self-explanatory.'
+    value "GENRE", value: 'Genre', description: 'Self-explanatory.'
+    value "PLATFORM", value: 'Platform', description: 'Self-explanatory.'
+    value "SERIES", value: 'Series', description: 'Self-explanatory.'
+    value "USER", value: 'User', description: 'Self-explanatory.'
   end
 end
