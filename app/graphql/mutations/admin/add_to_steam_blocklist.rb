b5a0d975c5ae2173a7b446023538d099429ed233
Connor Shea <connor.james.shea@gmail.com>
2021-05-01 14:28:13 -0600

    Update AddToSteamBlocklist mutation to destroy record if it exists.
    We don't want to have an existing SteamAppId record for this anymore
    if we're adding it to the blocklist.

--- app/graphql/mutations/admin/add_to_steam_blocklist.rb
+++ app/graphql/mutations/admin/add_to_steam_blocklist.rb
@@ -1,6 +1,6 @@
 # typed: true
 class Mutations::Admin::AddToSteamBlocklist < Mutations::BaseMutation
-  description "Add game to Steam blocklist. **Only available to admins using a first-party OAuth Application.**"
+  description "Add game to Steam blocklist and remove the Steam App ID from any existing games that have it. **Only available to admins using a first-party OAuth Application.**"
 
   argument :name, String, required: true, description: 'The name of the game being added to the blocklist.'
   argument :steam_app_id, Integer, required: true, description: 'ID of the Steam game.'
@@ -9,6 +9,10 @@ class Mutations::Admin::AddToSteamBlocklist < Mutations::BaseMutation
 
   sig { params(name: String, steam_app_id: Integer).returns(T::Hash[Symbol, SteamBlocklist]) }
   def resolve(name:, steam_app_id:)
+    # Find the existing SteamAppId record if it exists, and destroy it.
+    steam_app_id_record = SteamAppId.find_by(app_id: steam_app_id)
+    steam_app_id_record&.destroy
+
     steam_blocklist_entry = SteamBlocklist.create(user_id: @context[:current_user]&.id, name: name, steam_app_id: steam_app_id)
 
     raise GraphQL::ExecutionError, steam_blocklist_entry.errors.full_messages.join(", ") unless steam_blocklist_entry.save
