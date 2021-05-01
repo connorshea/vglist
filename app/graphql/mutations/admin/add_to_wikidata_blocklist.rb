708c988bd9252b5c8c1b255a58a70c2faa8a5686
Connor Shea <connor.james.shea@gmail.com>
2021-05-01 14:26:00 -0600

    Update AddToWikidataBlocklist mutation.
    It wasn't removing the Wikidata ID from the existing game, which
    was causing problems when actually calling the mutation.

--- app/graphql/mutations/admin/add_to_wikidata_blocklist.rb
+++ app/graphql/mutations/admin/add_to_wikidata_blocklist.rb
@@ -1,6 +1,6 @@
 # typed: true
 class Mutations::Admin::AddToWikidataBlocklist < Mutations::BaseMutation
-  description "Add game to Wikidata blocklist. **Only available to admins using a first-party OAuth Application.**"
+  description "Add game to Wikidata blocklist and remove Wikidata ID from any game that current has it. **Only available to admins using a first-party OAuth Application.**"
 
   argument :name, String, required: true, description: 'The name of the game being added to the blocklist.'
   argument :wikidata_id, Integer, required: true, description: 'ID of the Wikidata item.'
@@ -9,6 +9,11 @@ class Mutations::Admin::AddToWikidataBlocklist < Mutations::BaseMutation
 
   sig { params(name: String, wikidata_id: Integer).returns(T::Hash[Symbol, WikidataBlocklist]) }
   def resolve(name:, wikidata_id:)
+    # Find the existing game with this Wikidata ID and remove it from the
+    # record, if it exists.
+    game = Game.find_by(wikidata_id: wikidata_id)
+    game&.update!(wikidata_id: nil)
+
     wikidata_blocklist_entry = WikidataBlocklist.create(user_id: @context[:current_user]&.id, name: name, wikidata_id: wikidata_id)
 
     raise GraphQL::ExecutionError, wikidata_blocklist_entry.errors.full_messages.join(", ") unless wikidata_blocklist_entry.save
