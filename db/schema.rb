4f0d9d689a8754c60f6359bc5e8978611c10d93b
Connor Shea <connor.james.shea@gmail.com>
2021-12-28 11:42:51 -0700

    Add migration from Rails 7 upgrade.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_185728) do
+ActiveRecord::Schema.define(version: 2021_12_28_183617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_07_10_185728) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.string "checksum"
     t.datetime "created_at", null: false
     t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
