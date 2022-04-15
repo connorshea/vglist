61baa0fb14c5a35fb172db5715e2f23fc8d14c9e
Connor Shea <connor.james.shea@gmail.com>
2022-04-14 22:51:32 -0600

    Create a SQL view for Grouped Unmatched Games.
    It was hard to create a query that ActiveRecord didn't lose its mind
    over, so instead I'll use a view.

---
+++ db/migrate/20220415044554_create_grouped_unmatched_games_view.rb
@@ -0,0 +1,23 @@
+# typed: true
+class CreateGroupedUnmatchedGamesView < ActiveRecord::Migration[6.1]
+  def up
+    execute <<~SQL
+      CREATE OR REPLACE VIEW public.grouped_unmatched_games AS
+        SELECT
+          COUNT(*),
+          external_service_id,
+          external_service_name,
+          array_agg(name) as names
+        FROM
+          unmatched_games
+        GROUP BY
+          external_service_id,
+          external_service_name
+        ORDER BY COUNT DESC;
+    SQL
+  end
+
+  def down
+    execute('DROP VIEW IF EXISTS public.grouped_unmatched_games;')
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_15_022658) do
+ActiveRecord::Schema.define(version: 2022_04_15_044554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
