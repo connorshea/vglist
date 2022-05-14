70008cbda0504d69b420910a36bb5eb5357f5525
Connor Shea <connor.james.shea@gmail.com>
2022-05-14 15:31:01 -0600

    Split Live Statistics into its own GraphQL type.
    This refactors the shared fields from the two main statistics types into
    an interface so they don't need to both define all these fields. The
    live statistics using the same GraphQL type made the type signatures
    kinda wonky, and with this change the SiteStatisticType won't ever have
    a null ID or timestamp.

---
+++ app/graphql/types/site_statistic_item.rb
@@ -0,0 +1,39 @@
+module Types::SiteStatisticItem
+  include Types::BaseInterface
+
+  description "Site statistics attributes"
+
+  field :users, Integer, null: false, description: "The number of Users at this point in time."
+  field :games, Integer, null: false, description: "The number of Games at this point in time."
+  field :platforms, Integer, null: false, description: "The number of Platforms at this point in time."
+  field :series, Integer, null: false, description: "The number of Series at this point in time."
+  field :engines, Integer, null: false, description: "The number of Engines at this point in time."
+  field :companies, Integer, null: false, description: "The number of Companies at this point in time."
+  field :genres, Integer, null: false, description: "The number of Genres at this point in time."
+  field :stores, Integer, null: false, description: "The number of Stores at this point in time."
+  field :events, Integer, null: false, description: "The number of Events at this point in time."
+  field :game_purchases, Integer, null: false, description: "The number of Game Purchases at this point in time."
+  field :relationships, Integer, null: false, description: "The number of Relationships at this point in time."
+  field :games_with_covers, Integer, null: false, description: "The number of Games with covers at this point in time."
+  field :games_with_release_dates, Integer, null: false, description: "The number of Games with release dates at this point in time."
+  field :banned_users, Integer, null: false, description: "The number of Banned Users at this point in time."
+  field :unmatched_games, Integer, null: true, description: "The number of Unmatched Games collected via imports from external services (e.g. Steam) at this point in time."
+
+  # External IDs
+  field :mobygames_ids, Integer, null: false, description: "The number of MobyGames IDs at this point in time."
+  field :pcgamingwiki_ids, Integer, null: false, description: "The number of PCGamingWiki IDs at this point in time."
+  field :wikidata_ids, Integer, null: false, description: "The number of Wikidata IDs at this point in time."
+  field :giantbomb_ids, Integer, null: false, description: "The number of GiantBomb IDs at this point in time."
+  field :steam_app_ids, Integer, null: false, description: "The number of Steam App IDs at this point in time."
+  field :epic_games_store_ids, Integer, null: false, description: "The number of Epic Games Store IDs at this point in time."
+  field :gog_ids, Integer, null: false, description: "The number of GOG.com IDs at this point in time."
+  field :igdb_ids, Integer, null: true, description: "The number of IGDB IDs at this point in time."
+
+  # Versions
+  field :company_versions, Integer, null: true, description: "The number of Company Versions at this point in time."
+  field :game_versions, Integer, null: true, description: "The number of Game Versions at this point in time."
+  field :genre_versions, Integer, null: true, description: "The number of Genre Versions at this point in time."
+  field :engine_versions, Integer, null: true, description: "The number of Engine Versions at this point in time."
+  field :platform_versions, Integer, null: true, description: "The number of Platform Versions at this point in time."
+  field :series_versions, Integer, null: true, description: "The number of Series Versions at this point in time."
+end
