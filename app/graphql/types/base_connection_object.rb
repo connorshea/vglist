f0ffa806194ed1d102aed2df1c0683ca9015792a
Connor Shea <connor.james.shea@gmail.com>
2022-05-15 11:22:25 -0600

    Make the default connection object's nodes attribute non-nullable.
    This was causing a lot of annoying type errors in the frontend, and
    as far as I can tell this doesn't cause any issues with existing
    behaviors.

--- app/graphql/types/base_connection_object.rb
+++ app/graphql/types/base_connection_object.rb
@@ -5,6 +5,8 @@ class Types::BaseConnectionObject < GraphQL::Types::Relay::BaseConnection
   field :page_info, Types::PageInfoType, null: false, description: "Information to aid in pagination."
   field :total_count, Integer, null: false, description: "The total number of records returned by this query."
 
+  node_nullable(false)
+
   sig { returns(Integer) }
   def total_count
     object.items.size
