9caf23a0682315140d80fbc40b87866635a5bdb1
Connor Shea <connor.james.shea@gmail.com>
2022-05-30 16:07:59 -0600

    Fix GraphQL lint error.

--- app/graphql/types/enums/unmatched_game_external_service_type.rb
+++ app/graphql/types/enums/unmatched_game_external_service_type.rb
@@ -3,6 +3,6 @@ module Types::Enums
   class UnmatchedGameExternalServiceType < Types::BaseEnum
     description "External services that an Unmatched Game can come from, currently only Steam."
 
-    value "STEAM", value: 'Steam'
+    value "STEAM", value: 'Steam', description: 'Imported from the Steam store.'
   end
 end
