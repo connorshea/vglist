f1f5012eaab74fbcdcf285920064e73e323fcab5
Connor Shea <connor.james.shea@gmail.com>
2022-06-11 18:40:52 -0600

    Create migration to move all the events records over to the new tables.

---
+++ db/migrate/20220612003054_move_events_records_to_new_tables.rb
@@ -0,0 +1,31 @@
+# typed: true
+# Move the events records from the `events` table to the new separate tables
+# per-eventable type.
+class MoveEventsRecordsToNewTables < ActiveRecord::Migration[6.1]
+  def up
+    columns = [:user_id, :eventable_id, :created_at, :updated_at, :event_category]
+
+    Events::FavoriteGameEvent.insert_all(
+      Event.where(eventable_type: 'FavoriteGame').as_json(only: columns)
+    )
+
+    Events::RelationshipEvent.insert_all(
+      Event.where(eventable_type: 'Relationship').as_json(only: columns)
+    )
+
+    Events::UserEvent.insert_all(
+      Event.where(eventable_type: 'User').as_json(only: columns)
+    )
+
+    Events::GamePurchaseEvent.insert_all(
+      Event.where(eventable_type: 'GamePurchase').as_json(only: columns.concat([:differences]))
+    )
+  end
+
+  def down
+    Events::FavoriteGameEvent.destroy_all
+    Events::RelationshipEvent.destroy_all
+    Events::UserEvent.destroy_all
+    Events::GamePurchaseEvent.destroy_all
+  end
+end
