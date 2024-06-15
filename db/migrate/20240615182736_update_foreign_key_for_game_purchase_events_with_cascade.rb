61119f688614cf402760805b1e5e3eb4b31da7e7
Connor Shea <connor.james.shea@gmail.com>
2024-06-15 12:37:23 -0600

    Move deletion cascade into database.
    Handle this via Postgres rather than Rails, to avoid some weird behavior
    in Rails 7.1.

---
+++ db/migrate/20240615182736_update_foreign_key_for_game_purchase_events_with_cascade.rb
@@ -0,0 +1,10 @@
+# typed: true
+class UpdateForeignKeyForGamePurchaseEventsWithCascade < ActiveRecord::Migration[7.1]
+  def change
+    # Remove the existing foreign key
+    remove_foreign_key :events_game_purchase_events, :game_purchases
+
+    # Add the new foreign key with ON DELETE CASCADE
+    add_foreign_key :events_game_purchase_events, :game_purchases, column: :eventable_id, on_delete: :cascade
+  end
+end
