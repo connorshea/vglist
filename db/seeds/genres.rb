2bc20da96c0185f1279174f1c8dffc1268f9897d
Connor Shea <connor.james.shea@gmail.com>
2024-06-16 11:06:09 -0600

    Update seeds to include wikidata_id

--- db/seeds/genres.rb
+++ db/seeds/genres.rb
@@ -4,6 +4,7 @@ puts "Creating Genres..."
 # Create 20 unique genres.
 20.times do
   Genre.create!(
-    name: Faker::Game.unique.genre
+    name: Faker::Game.unique.genre,
+    wikidata_id: Faker::Number.unique.number(digits: 6)
   )
 end
--- db/seeds/series.rb
+++ db/seeds/series.rb
@@ -4,6 +4,7 @@ puts "Creating Series..."
 # Create 20 Series.
 20.times do
   Series.create!(
-    name: Faker::Game.unique.series
+    name: Faker::Game.unique.series,
+    wikidata_id: Faker::Number.unique.number(digits: 6)
   )
 end
