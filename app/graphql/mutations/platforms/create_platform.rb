c6ad0400cff608425bcae745b2f0fa515bc7559c
Connor Shea <connor.james.shea@gmail.com>
2024-06-16 10:58:40 -0600

    Update GraphQL arguments to correctly require wikidata_id.
    We now require Wikidata IDs on these models, so they have to be included
    when creating these records.

--- app/graphql/mutations/platforms/create_platform.rb
+++ app/graphql/mutations/platforms/create_platform.rb
@@ -3,12 +3,12 @@ class Mutations::Platforms::CreatePlatform < Mutations::BaseMutation
   description "Create a new game platform. **Only available to moderators and admins using a first-party OAuth Application.**"
 
   argument :name, String, required: true, description: 'The name of the platform.'
-  argument :wikidata_id, ID, required: false, description: 'The ID of the platform item in Wikidata.'
+  argument :wikidata_id, ID, required: true, description: 'The ID of the platform item in Wikidata.'
 
   field :platform, Types::PlatformType, null: true, description: "The platform that was created."
 
-  sig { params(name: String, wikidata_id: T.nilable(T.any(String, Integer))).returns(T::Hash[Symbol, Platform]) }
-  def resolve(name:, wikidata_id: nil)
+  sig { params(name: String, wikidata_id: T.any(String, Integer)).returns(T::Hash[Symbol, Platform]) }
+  def resolve(name:, wikidata_id:)
     platform = Platform.new(name: name, wikidata_id: wikidata_id)
 
     raise GraphQL::ExecutionError, platform.errors.full_messages.join(", ") unless platform.save
