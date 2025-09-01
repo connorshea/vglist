a28eed1b35b02f3fc3f07e52e976e0dcf4bb3950
Connor Shea <connor.james.shea@gmail.com>
2025-09-01 13:09:56 -0600

    Disable /dev/shm/ usage to work around potential problems with it running out of memory.
    Not really sure if this will fix the problem, but we can try.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -122,6 +122,7 @@ Capybara.register_driver :ci_chrome do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--headless')
   options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
 
   Capybara::Selenium::Driver.new app,
     browser: :chrome,
