3d6c7dda52fef4b61003053e9366cbd868b01526
Connor Shea <connor.james.shea@gmail.com>
2025-09-06 22:43:02 -0600

    Remove resolve-url-loader which we don't use as far as I know.
    Also add some CSS handling code to the webpack config. We intentionally do not want to use the cssbundling gem (at least right now, because we've implemented this stuff via the jsbundling webpack config for right now).

--- config/webpack/environment.js
+++
@@ -1,7 +0,0 @@
-const { environment } = require('@rails/webpacker');
-
-environment.loaders.get('sass').use.splice(-1, 0, {
-  loader: 'resolve-url-loader'
-});
-
-module.exports = environment;
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,20 @@ const path    = require("path")
 const { VueLoaderPlugin } = require('vue-loader')
 const webpack = require("webpack")
 
+// Extracts CSS into .css file
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// Removes exported JavaScript files from CSS-only entries
+// in this example, entry.custom will create a corresponding empty custom.js file
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+
 module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    application: [
+      "./app/javascript/application.js",
+      "./app/assets/stylesheets/application.scss",
+    ]
   },
   output: {
     filename: "[name].js",
