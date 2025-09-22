389c52791a1839463eaac369c6755ca276480a58
Connor Shea <connor.james.shea@gmail.com>
2025-09-21 18:18:00 -0600

    Fix up vue-loader.ts a bit.

--- app/javascript/src/vue-loader.ts
+++ app/javascript/src/vue-loader.ts
@@ -1,10 +1,16 @@
 import Vue, { h } from 'vue';
 
+/**
+ * This file is responsible for loading Vue components whenever we change pages
+ * and an element with `data-vue-component` is there.
+ *
+ * It is somewhat cursed.
+ */
 (async function() {
   const callback = () => {
     const elems = Array.from(document.querySelectorAll('[data-vue-component]'));
-    elems.forEach(async elem => {
-      const el = elem as HTMLElement;
+    elems.forEach(async (elem: HTMLElement) => {
+      const el = elem;
       const compName = el.dataset.vueComponent;
       const comp$ = await import(`./components/${compName}.vue`);
       const comp = comp$.default;
@@ -13,12 +19,10 @@ import Vue, { h } from 'vue';
         props = JSON.parse(el.dataset.vueProps);
       }
       // console.log(`Loaded Vue "${compName}", rendering...`, { comp, props });
+      // TODO: Make this use createApp() when we upgrade to Vue 3
       new Vue({
         el: elem,
-        render: () =>
-          h(comp, {
-            props: props
-          })
+        render: () => h(comp, { props: props })
       });
     });
   };
