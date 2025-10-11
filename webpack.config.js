77ec018aaf81eb231dfd88b36664943b2b747fae
Connor Shea <connor.james.shea@gmail.com>
2025-10-11 12:05:34 -0600

    Remove tsx from swc-loader.
    No need to support it since we don't have any tsx syntax.

--- webpack.config.js
+++ webpack.config.js
@@ -77,7 +77,7 @@ const config = {
               jsc: {
                 parser: {
                   syntax: "typescript",
-                  tsx: true
+                  tsx: false
                 }
               }
             },
