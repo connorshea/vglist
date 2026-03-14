36a0a0d879862586b946eb3e1302b745b55dfb1b
Connor Shea <connor.james.shea@gmail.com>
2026-03-14 13:58:53 -0600

    Update activity API specs.

--- spec/requests/api/activity_spec.rb
+++ spec/requests/api/activity_spec.rb
@@ -136,6 +136,105 @@ RSpec.describe "Activity API", type: :request do
       )
     end
 
+    it "returns eventable fields used by the activity feed frontend" do
+      game_purchase
+      relationship
+      favorite_game
+
+      query_string = <<-GRAPHQL
+        query {
+          activity(feedType: GLOBAL) {
+            nodes {
+              eventCategory
+              user {
+                id
+                username
+                slug
+              }
+              eventable {
+                ... on GamePurchase {
+                  game { id name }
+                  completionStatus
+                }
+                ... on FavoriteGame {
+                  game { id name }
+                }
+                ... on Relationship {
+                  followed { id username slug }
+                }
+                ... on User {
+                  id
+                  username
+                  slug
+                }
+              }
+            }
+          }
+        }
+      GRAPHQL
+
+      result = api_request(query_string, token: access_token)
+      nodes = result.graphql_dig(:activity, :nodes)
+
+      # Find nodes by event category
+      favorite_node = nodes.find { |n| n[:eventCategory] == "FAVORITE_GAME" }
+      relationship_node = nodes.find { |n| n[:eventCategory] == "FOLLOWING" }
+      add_to_library_node = nodes.find { |n| n[:eventCategory] == "ADD_TO_LIBRARY" }
+      new_user_nodes = nodes.select { |n| n[:eventCategory] == "NEW_USER" }
+
+      # GamePurchase eventable returns game name and id
+      expect(add_to_library_node[:eventable][:game][:name]).to eq(game_purchase.game.name)
+      expect(add_to_library_node[:eventable][:game][:id]).to eq(game_purchase.game.id.to_s)
+
+      # FavoriteGame eventable returns game name and id
+      expect(favorite_node[:eventable][:game][:name]).to eq(favorite_game.game.name)
+      expect(favorite_node[:eventable][:game][:id]).to eq(favorite_game.game.id.to_s)
+
+      # Relationship eventable returns followed user details
+      expect(relationship_node[:eventable][:followed][:username]).to eq(user2.username)
+      expect(relationship_node[:eventable][:followed][:slug]).to eq(user2.slug)
+
+      # User (NEW_USER) eventable returns user details
+      expect(new_user_nodes).not_to be_empty
+      new_user_node = new_user_nodes.first
+      expect(new_user_node[:eventable][:username]).to be_present
+      expect(new_user_node[:eventable][:slug]).to be_present
+    end
+
+    it "returns completionStatus on GamePurchase eventable when set" do
+      game_purchase_with_status = create(:game_purchase, :completion_status, user: user)
+      # Trigger the event by creating the game purchase above (which auto-creates an event)
+
+      query_string = <<-GRAPHQL
+        query {
+          activity(feedType: GLOBAL) {
+            nodes {
+              eventCategory
+              eventable {
+                ... on GamePurchase {
+                  game { id name }
+                  completionStatus
+                }
+              }
+            }
+          }
+        }
+      GRAPHQL
+
+      result = api_request(query_string, token: access_token)
+      nodes = result.graphql_dig(:activity, :nodes)
+
+      add_to_library_node = nodes.find do |n|
+        n[:eventCategory] == "ADD_TO_LIBRARY" &&
+          n.dig(:eventable, :game, :id) == game_purchase_with_status.game.id.to_s
+      end
+
+      expect(add_to_library_node).not_to be_nil
+      expect(add_to_library_node.dig(:eventable, :completionStatus)).to eq(
+        game_purchase_with_status.completion_status.upcase
+      )
+    end
+
     it "returns basic data for 'following' activity" do
       user
       user2
@@ -187,6 +286,75 @@ RSpec.describe "Activity API", type: :request do
           }
         ]
       )
+    end
+
+    it "returns following feed with eventable details" do
+      relationship
+      query_string = <<-GRAPHQL
+        query {
+          activity(feedType: FOLLOWING) {
+            nodes {
+              eventCategory
+              eventable {
+                ... on Relationship {
+                  followed { id username slug }
+                }
+                ... on User {
+                  id
+                  username
+                  slug
+                }
+              }
+            }
+          }
+        }
+      GRAPHQL
+
+      result = api_request(query_string, token: access_token)
+      nodes = result.graphql_dig(:activity, :nodes)
+
+      relationship_node = nodes.find { |n| n[:eventCategory] == "FOLLOWING" }
+      expect(relationship_node[:eventable][:followed][:username]).to eq(user2.username)
+      expect(relationship_node[:eventable][:followed][:slug]).to eq(user2.slug)
+    end
+
+    it "excludes private users from global activity feed" do
+      private_user = create(:private_user)
+      create(:game_purchase, user: private_user)
+
+      query_string = <<-GRAPHQL
+        query {
+          activity(feedType: GLOBAL) {
+            nodes {
+              user { username }
+            }
+          }
+        }
+      GRAPHQL
+
+      result = api_request(query_string, token: access_token)
+      nodes = result.graphql_dig(:activity, :nodes)
+      usernames = nodes.map { |n| n.dig(:user, :username) }
+
+      expect(usernames).not_to include(private_user.username)
+    end
+
+    it "returns an error when querying following feed without authentication" do
+      query_string = <<-GRAPHQL
+        query {
+          activity(feedType: FOLLOWING) {
+            nodes {
+              eventCategory
+            }
+          }
+        }
+      GRAPHQL
+
+      post graphql_path,
+        params: { query: query_string }
+
+      json = JSON.parse(response.body)
+      expect(json['errors'].first['message']).to eq("You must be logged in to view the following feed.")
     end
   end
 end
