91669abe2432853030d8ea3bce3e082a1e11f146
Connor Shea <connor.james.shea@gmail.com>
2026-03-14 12:20:22 -0600

    Fix the JWT.

--- app/services/jwt_service.rb
+++ app/services/jwt_service.rb
@@ -18,6 +18,6 @@ class JwtService
   end
 
   def self.secret_key
-    Rails.application.credentials.secret_key_base
+    Rails.application.credentials.secret_key_base || Rails.application.secret_key_base
   end
 end
