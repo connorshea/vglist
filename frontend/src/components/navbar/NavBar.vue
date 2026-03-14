436cb5e1791417f764f52350ec92860cd4f1857a
Connor Shea <connor.james.shea@gmail.com>
2026-03-13 22:56:22 -0600

    Get the home page working properly-ish.

--- frontend/src/components/navbar/NavBar.vue
+++ frontend/src/components/navbar/NavBar.vue
@@ -1,7 +1,14 @@
 <template>
   <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <router-link class="navbar-item has-text-weight-bold" to="/"> vglist </router-link>
+      <router-link class="navbar-item" to="/">
+        <img src="@/assets/images/vglist-logo.svg" alt="vglist" style="height: 24px" />
+      </router-link>
+
+      <div class="navbar-item is-hidden-touch">
+        <NavSearch />
+      </div>
+
       <a
         role="button"
         class="navbar-burger"
@@ -19,34 +26,45 @@
 
     <div class="navbar-menu" :class="{ 'is-active': isBurgerActive }">
       <div class="navbar-start">
-        <router-link class="navbar-item" to="/games">Games</router-link>
-        <router-link class="navbar-item" to="/platforms">Platforms</router-link>
-        <router-link class="navbar-item" to="/genres">Genres</router-link>
-        <router-link class="navbar-item" to="/companies">Companies</router-link>
-        <router-link class="navbar-item" to="/engines">Engines</router-link>
-        <router-link class="navbar-item" to="/series">Series</router-link>
-        <router-link class="navbar-item" to="/stores">Stores</router-link>
-        <router-link class="navbar-item" to="/activity">Activity</router-link>
-
-        <div class="navbar-item">
+        <div class="navbar-item is-hidden-desktop">
           <NavSearch />
+        </div>
+
+        <router-link class="navbar-item" to="/activity">Activity</router-link>
+        <router-link class="navbar-item" to="/games">Games</router-link>
+        <router-link class="navbar-item" to="/users">Users</router-link>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">More</a>
+          <div class="navbar-dropdown">
+            <router-link class="navbar-item" to="/platforms">Platforms</router-link>
+            <router-link class="navbar-item" to="/genres">Genres</router-link>
+            <router-link class="navbar-item" to="/companies">Companies</router-link>
+            <router-link class="navbar-item" to="/engines">Engines</router-link>
+            <router-link class="navbar-item" to="/series">Series</router-link>
+            <router-link class="navbar-item" to="/stores">Stores</router-link>
+            <hr class="navbar-divider" />
+            <router-link class="navbar-item" to="/about">About</router-link>
+          </div>
         </div>
       </div>
 
       <div class="navbar-end">
         <template v-if="authStore.isAuthenticated">
-          <router-link class="navbar-item" :to="`/users/${authStore.user?.slug}`">
-            {{ authStore.user?.username }}
-          </router-link>
           <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">More</a>
+            <a class="navbar-link">
+              {{ authStore.user?.username }}
+            </a>
             <div class="navbar-dropdown is-right">
-              <router-link class="navbar-item" to="/settings">Settings</router-link>
+              <router-link class="navbar-item" :to="`/users/${authStore.user?.slug}`">
+                Profile
+              </router-link>
               <router-link v-if="authStore.isAdmin" class="navbar-item" to="/admin">
                 Admin
               </router-link>
+              <router-link class="navbar-item" to="/settings">Settings</router-link>
               <hr class="navbar-divider" />
-              <a class="navbar-item" @click="handleSignOut">Sign Out</a>
+              <a class="navbar-item" @click="handleSignOut">Sign out</a>
             </div>
           </div>
         </template>
