15b9321fd480f4faa097fee1d2e74d0575a42a28
Connor Shea <connor.james.shea@gmail.com>
2026-03-14 20:45:52 -0600

    Fix N+1s and add a few specs.

--- spec/models/views/new_event_spec.rb
+++ spec/models/views/new_event_spec.rb
@@ -29,6 +29,65 @@ RSpec.describe Views::NewEvent, type: :model do
     it { should belong_to(:user) }
   end
 
+  describe "Scopes" do
+    describe ".recently_created" do
+      let(:user) { create(:confirmed_user) }
+
+      it "returns events in descending created_at order" do
+        game_purchase1 = create(:game_purchase, user: user)
+        game_purchase2 = create(:game_purchase, user: user)
+
+        events = Views::NewEvent.recently_created.where(event_category: :add_to_library)
+        expect(events.first.created_at).to be >= events.last.created_at
+        expect(events.map(&:eventable_id)).to eq([game_purchase2.id, game_purchase1.id])
+      end
+
+      it "works when joined with users table without ambiguous column error" do
+        create(:game_purchase, user: user)
+
+        expect {
+          Views::NewEvent.recently_created.includes(:user).where(user_id: user.id).to_a
+        }.not_to raise_error
+      end
+    end
+  end
+
+  describe ".preload_eventables" do
+    let(:user) { create(:confirmed_user) }
+
+    it "preloads eventable data for a collection of events" do
+      game_purchase = create(:game_purchase, user: user)
+      favorite_game = create(:favorite_game, user: user)
+      relationship = create(:relationship, follower: user)
+
+      events = Views::NewEvent.where(user_id: user.id).to_a
+      result = Views::NewEvent.preload_eventables(events)
+
+      # Should return the same events
+      expect(result).to eq(events)
+
+      # Each event should have a preloaded subclass
+      result.each do |event|
+        expect(event.instance_variable_get(:@_preloaded_subclass)).not_to be_nil
+      end
+
+      # Eventables should be accessible without additional queries
+      add_event = result.find(&:add_to_library?)
+      expect(add_event.eventable).to eq(game_purchase)
+
+      fav_event = result.find(&:favorite_game?)
+      expect(fav_event.eventable).to eq(favorite_game)
+
+      follow_event = result.find(&:following?)
+      expect(follow_event.eventable).to eq(relationship)
+    end
+
+    it "returns an empty array when given no events" do
+      result = Views::NewEvent.preload_eventables([])
+      expect(result).to eq([])
+    end
+  end
+
   describe 'GamePurchase Event Destructions' do
     let(:user) { create(:confirmed_user) }
     let(:game) { create(:game) }
